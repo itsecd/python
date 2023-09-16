0edef23e92e34520a6e898b9c81200c649261461
Вероника Будюк <veronika.budyuk2004@gmail.com>
2023-09-16 22:08:54 +0400

    html-code for one pitures

--- Lab1/main.py
+++ Lab1/main.py
@@ -10,3 +10,10 @@ from decimal import Decimal
 import io
 from datetime import datetime
 
+#запросы и получение html кода страницы
+URL_ROSE="https://yandex.ru/images/search?text=rose.jpg"
+
+HTML_ROSE=requests.get(URL_ROSE).text
+
+soup_rose=BeautifulSoup(HTML_ROSE, 'lxml')
+#r=soup_rose.find('div', class_='serp-item__preview').find('a', class_='serp-item__link').get('href')
