fe2ecf58fbd37ebca29ca2926291667656fda601
Вероника Будюк <veronika.budyuk2004@gmail.com>
2023-09-16 17:59:59 +0400

    install bs and done requirements.txt

---
+++ Lab1/main.py
@@ -0,0 +1,12 @@
+# libaries
+import pandas as pd
+import numpy as np
+import time
+from bs4 import BeautifulSoup
+import requests
+import re
+from re import sub
+from decimal import Decimal
+import io
+from datetime import datetime
+
