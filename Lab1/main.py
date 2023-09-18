512db52fb3301b9bae9d9f5c3cc64eed89c3e471
Вероника Будюк <veronika.budyuk2004@gmail.com>
2023-09-18 15:52:45 +0400

    def for create folder ang html-code tulip

--- Lab1/main.py
+++ Lab1/main.py
@@ -5,15 +5,47 @@ import time
 from bs4 import BeautifulSoup
 import requests
 import re
+import os
 from re import sub
 from decimal import Decimal
 import io
 from datetime import datetime
 
-#запросы и получение html кода страницы
-URL_ROSE="https://yandex.ru/images/search?text=rose.jpg"
 
-HTML_ROSE=requests.get(URL_ROSE).text
+def create_directory(folder: str):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
-soup_rose=BeautifulSoup(HTML_ROSE, 'lxml')
-#r=soup_rose.find('div', class_='serp-item__preview').find('a', class_='serp-item__link').get('href')
+
+
+
+# запросы и получение html кода страницы
+URL_ROSE = "https://yandex.ru/images/search?text=rose.jpg" 
+URL_TULIP = "https://yandex.ru/images/search?from=tabbar&text=tulip.jpg"
+
+HTML_ROSE = requests.get(URL_ROSE).text
+HTML_TULIP = requests.get(URL_TULIP).text
+
+SOUP_ROSE = BeautifulSoup(HTML_ROSE, 'lxml')
+SOUP_TULIP = BeautifulSoup(HTML_TULIP, 'lxml')
+
+
+
+r = SOUP_ROSE.find('div', class_='serp-item__preview')  #.find('a', class_='serp-item__link').get('href')
+t = SOUP_ROSE.find('div', class_='serp-item__preview') #.find('a', class_='serp-item__link').get('href')
+
+print(r)
+print(t)
+create_directory("dataset")
+create_directory("dataset/rose")
+create_directory("dataset/tulip")
+
+
+
+
+
+
+
+
+# создание папки с помощью библиотеки os
+
