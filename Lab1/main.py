07cd5fe31ec6f24d009c8f77da1d030fd7c9135c
Вероника Будюк <veronika.budyuk2004@gmail.com>
2023-09-20 16:55:40 +0400

    all func, url for roses, do "for and while"

--- Lab1/main.py
+++ Lab1/main.py
@@ -3,6 +3,7 @@ import os
 from bs4 import BeautifulSoup
 import requests
 import cv2
+import shutil
 
 
 
@@ -13,18 +14,20 @@ def create_directory(folder: str):
 def download(url):
     try: 
         response=requests.get(url=url)
-        for i in range(1000):
+        for i in range(10):
          with open(f'dataset/rose/{i}.jpg', 'wb') as file:
             file.write(response.content)
     except: 'Uncorrect URL'
 
-#def delete(folder:str):
-        os.remove(folder) 
+def delete(folder:str):
+    shutil.rmtree(folder) 
+       
+
    
 
 # запросы и получение html кода страницы
-URL_ROSE = "https://yandex.ru/images/search?text=rose.jpg" 
-URL_TULIP = "https://yandex.ru/images/search?from=tabbar&text=tulip.jpg"
+URL_ROSE = "https://www.google.com/search?sca_esv=566872717&q=rose.jpg&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjk0ond4LiBAxUyExAIHadUCPMQ0pQJegQIEBAB&biw=1036&bih=810&dpr=1.25" 
+URL_TULIP = "https://www.bing.com/images/search?q=tulip.jpg&form=HDRSC2&first=1"
 
 HTML_ROSE = requests.get(URL_ROSE, headers={"User-Agent":"Mozilla/5.0"})
 HTML_TULIP = requests.get(URL_TULIP, headers={"User-Agent":"Mozilla/5.0"})
@@ -32,24 +35,24 @@ HTML_TULIP = requests.get(URL_TULIP, headers={"User-Agent":"Mozilla/5.0"})
 SOUP_ROSE = BeautifulSoup(HTML_ROSE.text, 'lxml')
 SOUP_TULIP = BeautifulSoup(HTML_TULIP.text, 'lxml')
 
+#rose=SOUP_ROSE.find('img', class_='DS1iW')
 
-
-r = SOUP_ROSE.find_all('img')
+#roses = SOUP_ROSE.find_all('img')
 t = SOUP_ROSE.find_all('img')
-URL='https://avatars.mds.yandex.net/i?id=17291e9f90aafdd133916ce64c0f3be9ccd19850-10311215-images-thumbs&n=13'
+
+data=[]
+roses=SOUP_ROSE.findAll('img', class_='DS1iW')
+#link=SOUP_ROSE.find('img', class_='DS1iW').get('src')
+for rose in roses:
+    link=SOUP_ROSE.find('img', class_='DS1iW').get('src')
+    data.append([link])
+   # download(img_url)
+   # img_data = requests.get(img_url).content
+   # img_filename = f"{i + 0000}.jpg"
 
 
-print(r)
-create_directory("dataset")
-create_directory("dataset/rose")
-create_directory("dataset/tulip")
-#download(url=URL)
-#cv2.imwrite('dataset/rose', 'i.jpg' )
-
-delete("dataset/rose")
+print(data)
+print(len(data))
 
 
-
-
-# создание папки с помощью библиотеки os
 
