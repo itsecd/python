970e24927cebca27a4f040741311d9c426f7e6cd
aidenpirs007@mail.ru <aidenpirs007@mail.ru>
2023-09-20 17:03:28 +0400

    Fixed capcha

--- Lab1/main.py
+++ Lab1/main.py
@@ -8,33 +8,36 @@ def create_directory(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+def get_html_page(url):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+    return response.text
+
 def download_img(search_query, save_directory, num_images=1000):
     create_directory(save_directory)
-    base_url = "https://yandex.ru/images/"
 
-    for start in range(0, num_images, 10):
-        params = {
-            "text": search_query,
-            "type": "photo",
-            "from": "tabbar",
-            "p": str(start),
-        }
+    for start in range(0, num_images, 100):
+        
+        base_url = f"https://www.bing.com/images/search?q={search_query}&form=HDRSC2&first={start}"
+        try:
+            response_text = get_html_page(base_url)  # Получаем HTML-страницу
+            soup = BeautifulSoup(response_text, "html.parser")
 
-        response = requests.get(base_url,params=params)
-        soup = BeautifulSoup(response.text, "html.parser")
+            img_tags = soup.find_all("img", class_="mimg")
+            for i, img_tag in enumerate(img_tags):
+                img_url = img_tag.get("src")
+                img_data = requests.get(img_url).content
+                img_filename = f"{i+start:04d}.jpg"
+                img_path = os.path.join(save_directory,img_filename)
 
-        img_tags = soup.find_all("img", class_="serp-item__thumb")
-        for i, img_tag in enumerate(img_tags):
-            img_url = img_tag.get("src")
-            img_data = requests.get(img_url).content
-            img_filename = f"{i+start:04d}.jpg"
-            img_path = os.path.join(save_directory,img_filename)
-            with open(img_path, "wb") as img_file:
-                img_file.write(img_data)
+                with open(img_path, "wb") as img_file:
+                    img_file.write(img_data)
 
-            print(f"Скачано изображение {img_filename}")
+                print(f"Скачано изображение {img_filename}")
+        except requests.exceptions.MissingSchema:
+            print(f"Проблема с URL: {base_url}")
 
-    print(f"Загружено {num_images} изображений")
+    print(f"Загружено {num_images} изображений класса {search_query}")
 
 classes = ["polar bear", "brown bear"]
 num_images_for_class = 1000
