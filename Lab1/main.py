a3e8f273e4a698612251eef0b7fed5b98abb7402
Вероника Будюк <veronika.budyuk2004@gmail.com>
2023-09-20 22:20:57 +0400

    web-scrapper done! but downloads only 40 pict ;((

--- Lab1/main.py
+++ Lab1/main.py
@@ -2,7 +2,6 @@
 import os
 from bs4 import BeautifulSoup
 import requests
-import cv2
 import shutil
 
 
@@ -21,13 +20,16 @@ def download(url, folder):
                 file.write(response.content)
                 count+=1
         except: 'Uncorrect URL'
+        if count+2>len(url):
+                break
+
 
 def delete(folder:str):
     shutil.rmtree(folder) 
        
 
 URL_ROSE = "https://www.google.com/search?sca_esv=566872717&q=rose.jpg&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjk0ond4LiBAxUyExAIHadUCPMQ0pQJegQIEBAB&biw=1036&bih=810&dpr=1.25" 
-URL_TULIP = "https://www.bing.com/images/search?q=tulip.jpg&form=HDRSC2&first=1"
+URL_TULIP = "https://www.google.com/search?q=tulip.jpg&tbm=isch&ved=2ahUKEwi71JmWmbmBAxWTJxAIHaCXBiMQ2-cCegQIABAA&oq=tulip.jpg&gs_lcp=CgNpbWcQARgAMgcIABATEIAEMggIABAHEB4QEzIICAAQBxAeEBMyCAgAEAcQHhATOgUIABCABDoGCAAQBxAeUKcXWIlMYOlcaARwAHgAgAH0BIgBigySAQYxMS41LTGYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=ceYKZfvXEpPPwPAPoK-amAI&bih=810&biw=1036"
 
 HTML_ROSE = requests.get(URL_ROSE, headers={"User-Agent":"Mozilla/5.0"})
 HTML_TULIP = requests.get(URL_TULIP, headers={"User-Agent":"Mozilla/5.0"})
@@ -35,17 +37,28 @@ HTML_TULIP = requests.get(URL_TULIP, headers={"User-Agent":"Mozilla/5.0"})
 SOUP_ROSE = BeautifulSoup(HTML_ROSE.text, 'lxml')
 SOUP_TULIP = BeautifulSoup(HTML_TULIP.text, 'lxml')
 
-t = SOUP_ROSE.find_all('img')
+roses=SOUP_ROSE.findAll('img')
+tulips=SOUP_TULIP.findAll('img')
 
-data=[]
-roses=SOUP_ROSE.findAll('img', class_='DS1iW')
-#link=SOUP_ROSE.find('img', class_='DS1iW').get('src')
-links=SOUP_ROSE.findAll('img')
+for rose in roses:
+    len_folder_roses = len(os.listdir(path="dataset/rose"))
+    len_roses=len(roses)
+    if len_folder_roses+2>len_roses:
+         break
+    else:
+        create_directory("dataset/rose")
+        download(roses, "dataset/rose")
+       
+    
 
-for link in links:
-     create_directory("dataset/rose")
-     download(links, "dataset/rose")
-
+for tulip in tulips:
+    len_folder_tulips = len(os.listdir(path="dataset/tulip"))
+    len_tulip=len(tulips)
+    if len_folder_tulips+2>len_tulip:
+         break
+    else:
+        create_directory("dataset/tulip")
+        download(tulips, "dataset/tulip")
 
 
 
