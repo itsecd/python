9eb19aa4e4759128de7d300d1ce036da545a9e9d
Yana Yakovleva <Yakovlevaya11.06@gmail.com>
2023-09-20 18:50:12 +0400

    rework download_images: add scrolling pages

--- Lab1/lab1-var6.py
+++ Lab1/lab1-var6.py
@@ -2,48 +2,45 @@ import os
 import requests
 import cv2
 from bs4 import BeautifulSoup
-
-def folder_create(images):
+"""
+def folder_create():
  try:
     folder_name=input("Enter Folder name:- ")
     os.mkdir(f'dataset/{folder_name}')
  except:
     print("Folder Exist with that name!")
     folder_create()
- download_images(images,f'dataset/{folder_name}')
-
-def download_images(images, folder_name):
+"""
+def download_images(url, folder_name,split):
+  r = requests.get(url,headers={"User-Agent":"Mozilla/5.0"})
+  soup = BeautifulSoup(r.text, 'lxml')
+  for p in range(1,6):
+    urls=f"https://www.yandex.ru/images/search?lr=51&p={p}&rpt=image&{split}"
+    images = soup.find_all('img')
   count=0
   print(f"Total {len(images)} Image Found!")
   if len(images) != 0:
-    for i, image in enumerate(images):
-      try:
-        image_link=image["src"]
-      except:
-        pass     
-      try:
-        r=requests.get(image_link).content
-        try:
-          r = str(r, 'utf-8')
-        except UnicodeDecodeError:
-          with open(f"{folder_name}/{i}.jpg", "wb+") as f:
-            f.write(r)
-          count+=1
-      except:
-        pass
-    if count==len(images):
-      print("All Images Downloaded!")
-    else:
-      print(f"Not all of {count} images are Downloaded")
+    count=0
+    for image in images[1:]:
+        src="https:"+image['src']
+        response=requests.get(src, allow_redirects=True)
+        with open(f"dataset/{folder_name}/{count}.jpg",'wb') as f:
+           f.write(response.content)
+           count+=1
+    if (count==len(images)):
+       print("All Images are Downloaded!")
 
-def main(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    images = soup.findAll('img')
-    folder_create(images)
+    else:
+       print("Houston, we have a problem")   
+
+
 os.mkdir('dataset')
 url = input("Enter site URL:")
-main(url)
+split=(url.split('?'))[1]
+folder_name=input("Enter Folder name:- ")
+os.mkdir(f'dataset/{folder_name}')
+download_images(url,folder_name,split)
           
-
+#URL for tigers: https://yandex.ru/images/search?text=tiger
+#URL for leopards: https://yandex.ru/images/search?text=leopard
          
