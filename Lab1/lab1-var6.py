24c950731fc7b02c5bb3de84d741019bc8dfe765
Yana Yakovleva <Yakovlevaya11.06@gmail.com>
2023-09-20 22:44:24 +0400

    working code(problem with captcha)

--- Lab1/lab1-var6.py
+++ Lab1/lab1-var6.py
@@ -2,45 +2,51 @@ import os
 import requests
 import cv2
 from bs4 import BeautifulSoup
-"""
-def folder_create():
- try:
-    folder_name=input("Enter Folder name:- ")
-    os.mkdir(f'dataset/{folder_name}')
- except:
-    print("Folder Exist with that name!")
-    folder_create()
-"""
-def download_images(url, folder_name,split):
-  r = requests.get(url,headers={"User-Agent":"Mozilla/5.0"})
-  soup = BeautifulSoup(r.text, 'lxml')
-  for p in range(1,6):
+
+#function for creating folders
+
+def create_folders(folder_name_first,folder_name_second):
+  os.mkdir('dataset')
+  try:
+    os.mkdir(f'dataset/{folder_name_first}')
+    os.mkdir(f'dataset/{folder_name_second}')
+  except:
+    print("Problem! Try again")
+
+#function for downloading images
+
+def download_images(folder_name,split):
+  k=0
+  for p in range(0,5):
     urls=f"https://www.yandex.ru/images/search?lr=51&p={p}&rpt=image&{split}"
+    r = requests.get(urls,headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'})
+    soup = BeautifulSoup(r.text, 'lxml')
     images = soup.find_all('img')
-  count=0
-  print(f"Total {len(images)} Image Found!")
-  if len(images) != 0:
-    count=0
-    for image in images[1:]:
-        src="https:"+image['src']
-        response=requests.get(src, allow_redirects=True)
-        with open(f"dataset/{folder_name}/{count}.jpg",'wb') as f:
-           f.write(response.content)
-           count+=1
-    if (count==len(images)):
-       print("All Images are Downloaded!")
-
+    if(len(images)==2):
+      print("Houston, we have a problem")
+      continue
     else:
-       print("Houston, we have a problem")   
+     count=0
+     i=k
+     for image in images[1:]:
+      src="https:"+image['src']
+      response=requests.get(src, headers={"User-Agent":"Mozilla/5.0"})
+      with open(f"dataset/{folder_name}/{format(i).zfill(4)}.jpg",'wb') as f:
+        f.write(response.content)
+        count+=1
+        i+=1
+     k+=(i-1) 
+     print(f"Total {k} Image Found!")
+   
+#calling functions
+url_tiger='https://yandex.ru/images/search?text=leopard'
+url_leopard='https://yandex.ru/images/search?text=leopard'
+split_tiger=(url_tiger.split('?'))[1]
+split_leopard=(url_leopard.split('?'))[1]
+folder_name_tiger="tigers"
+folder_name_leopard="leopards"
+create_folders(folder_name_tiger,folder_name_leopard)
+download_images(folder_name_tiger,split_tiger)
+download_images(folder_name_leopard,split_leopard)
 
 
-os.mkdir('dataset')
-url = input("Enter site URL:")
-split=(url.split('?'))[1]
-folder_name=input("Enter Folder name:- ")
-os.mkdir(f'dataset/{folder_name}')
-download_images(url,folder_name,split)
-          
-#URL for tigers: https://yandex.ru/images/search?text=tiger
-#URL for leopards: https://yandex.ru/images/search?text=leopard
-         
