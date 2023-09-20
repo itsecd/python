4523aa29f2d99c1c43a3f2bb80f7f51635506b37
aidenpirs007@mail.ru <aidenpirs007@mail.ru>
2023-09-20 15:30:02 +0400

    Downloading function is done

--- Lab1/main.py
+++ Lab1/main.py
@@ -23,4 +23,15 @@ def download_img(search_query, save_directory, num_images=1000):
         response = requests.get(base_url,params=params)
         soup = BeautifulSoup(response.text, "html.parser")
 
-        
+        img_tags = soup.find_all("img", class_="serp-item__thumb")
+        for i, img_tag in enumerate(img_tags):
+            img_url = img_tag.get("src")
+            img_data = requests.get(img_url).content
+            img_filename = f"{i+start:04d}.jpg"
+            img_path = os.path.join(save_directory,img_filename)
+            with open(img_path, "wb") as img_file:
+                img_file.write(img_data)
+
+            print(f"Скачано изображение {img_filename}")
+
+    print(f"Загружено {num_images} изображений")
