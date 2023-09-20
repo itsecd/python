b1c8bdc547c85c5b89b796e6da77659386818a83
Yana Yakovleva <Yakovlevaya11.06@gmail.com>
2023-09-20 12:44:03 +0400

    Add def folder_create

--- Lab1/lab1-var6.py
+++ Lab1/lab1-var6.py
@@ -3,11 +3,21 @@ import requests
 import cv2
 from bs4 import BeautifulSoup
 
+def folder_create(images):
+ try:
+    folder_name=input("Enter Folder name:- ")
+    os.mkdir(f'dataset/{folder_name}')
+ except:
+    print("Folder Exist with that name!")
+    folder_create()
+ download_images(images,f'dataset/{folder_name}')
+
 
 def main(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     images = soup.findAll('img')
+    folder_create(images)
 os.mkdir('dataset')
 url = input("Enter site URL:")
 main(url)
