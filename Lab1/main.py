7fd2f8f98ba5ac272716cc1030de6ba055d39bb0
Вероника Будюк <veronika.budyuk2004@gmail.com>
2023-09-28 22:39:00 +0400

    lab done, to do only pep

--- Lab1/main.py
+++ Lab1/main.py
@@ -4,30 +4,6 @@ import shutil
 from bs4 import BeautifulSoup
 import requests
 
-
-list_url_rose=[]
-count_rose=0
-for pages in range(1, 10):
-    url_rose= f"https://www.bing.com/images/search?q=rose.jpg&redig=7D4B2E55AA5E4A4CA223FB76FBC6D258&first={pages}"
-    html_rose=requests.get(url_rose, headers={"User-Agent":"Mozilla/5.0"})
-    soup_rose = BeautifulSoup(html_rose.text, 'lxml')
-    roses=soup_rose.findAll('img')
-    list_url_rose+=roses
-    count_rose+=len(roses)
-
-
-list_url_tulip=[]
-count_tulip=0
-for pages in range(1, 10):
-    url_tulip= f"https://www.bing.com/images/search?q=tulip.jpg&qs=UT&form=QBIR&sp=1&lq=0&pq=tulip.jpg&sc=2-9&cvid=9577F520591A403C88168B8637C22677&first={pages}"
-    html_tulip=requests.get(url_tulip, headers={"User-Agent":"Mozilla/5.0"})
-    soup_tulip = BeautifulSoup(html_tulip.text, 'lxml')
-    tulip=soup_tulip.findAll('img')
-    list_url_tulip+=tulip
-    count_tulip+=len(tulip)
-
-
-
 def create_directory(folder: str):
     if not os.path.exists(folder):
         os.makedirs(folder)
@@ -42,17 +18,42 @@ def download(url, folder):
                 file.write(response.content)
                 count+=1
         except: 'Uncorrect URL'
-        if count+2>len(url):
-                break
+        if count>30:
+            break
 
 def delete(folder:str):
     shutil.rmtree(folder) 
        
 
+
+list_url_rose=[]
+count_rose=0
+for pages in range(1, 10):
+    url_rose= f"https://www.bing.com/images/search?q=rose.jpg&redig=7D4B2E55AA5E4A4CA223FB76FBC6D258&first={pages}"
+    html_rose=requests.get(url_rose, headers={"User-Agent":"Mozilla/5.0"})
+    soup_rose = BeautifulSoup(html_rose.text, 'lxml')
+    roses=soup_rose.findAll('img', class_='mimg')
+    list_url_rose+=roses
+    if count_rose>30:
+        break
+
+
+
+list_url_tulip=[]
+count_tulip=0
+for pages in range(1, 10):
+    url_tulip= f"https://www.bing.com/images/search?q=tulip.jpg&qs=UT&form=QBIR&sp=1&lq=0&pq=tulip.jpg&sc=2-9&cvid=9577F520591A403C88168B8637C22677&first={pages}"
+    html_tulip=requests.get(url_tulip, headers={"User-Agent":"Mozilla/5.0"})
+    soup_tulip = BeautifulSoup(html_tulip.text, 'lxml')
+    tulip=soup_tulip.findAll('img', class_='mimg')
+    list_url_tulip+=tulip
+    if count_tulip>30:
+        break
+
+
 for rose in list_url_rose:
     len_folder_roses = len(os.listdir(path="dataset/rose"))
-    len_roses=len(list_url_rose)
-    if len_folder_roses+2>len_roses:
+    if len_folder_roses>30:
          break
     else:
         create_directory("dataset/rose")
@@ -60,8 +61,7 @@ for rose in list_url_rose:
        
 for tulip in list_url_tulip:
     len_folder_tulips = len(os.listdir(path="dataset/tulip"))
-    len_tulip=len(list_url_tulip)
-    if len_folder_tulips+2>len_tulip:
+    if len_folder_tulips>30:
          break
     else:
         create_directory("dataset/tulip")
