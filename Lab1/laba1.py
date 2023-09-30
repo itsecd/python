96ebedec3858ccfc2f77e139b455621be9997956
Борис Шестериков <boris20033@gmail.com>
2023-09-30 17:25:36 +0400

    made search def for flowers

--- Lab1/laba1.py
+++ Lab1/laba1.py
@@ -2,41 +2,58 @@ from bs4 import BeautifulSoup
 import requests
 import os
 
+COUNT = 2000
+
 def create_folder(folder : str):
     if not os.path.isdir(folder): 
-        os.mkdir(folder)
+        os.makedirs(folder)
 
-def download(src, folder : str):
+def download(urls, folder : str):
     count = 0
-    for rose in src:
-        src = rose["src"]
-        response = requests.get(src)
-        with open(f"{folder}/{count}.jpg", "wb") as file:
-            file.write(response.content)
-            count+=1
+    for flower_url in urls:
+        try:
+            src=flower_url['src']
+            response = requests.get(src)
+            with open(f"{folder}/{count:04}.jpg", "wb") as file:
+                file.write(response.content)
+                count+=1
+        except: "Uncorect URL" 
+        if(count >= COUNT):
+            break
     
 
-
-url_rose = "https://yandex.ru/images/search?lr=51&text=rose.jpg"
-url_tulip = "https://yandex.ru/images/search?lr=51&text=tulip.jpg"
-
-headers = {
-    "Accept" : "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
-    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+_headers = {
+   "User-Agent":"Mozilla/5.0"
 }
 
-html_rose = requests.get(url_rose, headers=headers)
-html_tulip = requests.get(url_tulip, headers=headers)
+roses_list_url = []
+tulips_list_url = []
 
-soup_rose = BeautifulSoup(html_rose.text, "lxml") 
-soup_tulip = BeautifulSoup(html_tulip.text, "lxml") 
 
-roses_list = soup_rose.find_all("img", class_="serp-item__thumb justifier__thumb")
-tulips_list = soup_tulip.find_all("img", class_="serp-item__thumb justifier__thumb")
+for page in range(1, 999):
+    url_rose = ("https://www.bing.com/images/search?q=rose.jpg&go=%D0%9F%D0%B"+ 
+    "E%D0%B8%D1%81%D0%BA&qs=ds&form=QBIR&first={page}")
+    url_tulip = ("https://www.bing.com/images/search?q=tulip.jpg&qs=n&form=QB"+ 
+    "IR&sp=-1&lq=0&pq=tulip.jp&sc=6-8&cvid=973A3A4B92834C4B8848B29F7D753069&g"+ 
+    "hsh=0&ghacc=0&first={page}")
 
-create_folder("Lab1/dataset/roses")
+    html_rose = requests.get(url_rose, headers=_headers) # Get a html code
+    html_tulip = requests.get(url_tulip, headers=_headers) 
+
+    soup_rose = BeautifulSoup(html_rose.text, "lxml") # Get a BS object
+    soup_tulip = BeautifulSoup(html_tulip.text, "lxml") 
+
+    roses_list_url += soup_rose.find_all("img", class_="mimg") # Get a list of <img> 
+    tulips_list_url += soup_tulip.find_all("img", class_="mimg")
+
+    if(len(roses_list_url) >= COUNT and len(tulips_list_url) >= COUNT):
+        break
+
+create_folder("Lab1/dataset/roses") # Create folders
 create_folder("Lab1/dataset/tulip")
 
-download(roses_list, "Lab1/dataset/roses")
-download(tulips_list, "Lab1/dataset/tulip")
-# print(roses_list)
+download(roses_list_url, "Lab1/dataset/roses") # Download images
+download(tulips_list_url, "Lab1/dataset/tulip")
+
+print(len(roses_list_url))
+print(len(tulips_list_url))
