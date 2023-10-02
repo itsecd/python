5c3d0a502cade3ed5f46756e50b85a267a83f487
Влад Цуненко <vlad.tsunenko.2004@mail.ru>
2023-10-02 18:49:02 +0400

    working for 1 day

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -10,4 +10,12 @@ start_date = end_date - delta
 url = 'https://www.cbr-xml-daily.ru/archive/'+str(start_date).replace("-","/")+'/daily_json.js'
 response = requests.get(url)
 
-print(url)
+if response.status_code == 200:
+        data = response.json()
+    
+        usd_rates = data["Valute"]["USD"]["Value"]
+
+        with open("dataset.csv", "a", newline="", encoding="utf-8") as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter = ",")
+
+            csv_writer.writerow([start_date, usd_rates])
