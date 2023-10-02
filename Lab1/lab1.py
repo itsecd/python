05225e27729c847edfae6922141cbaf89ab5dfc3
Darktezi <Darktezi@mail.ru>
2023-10-02 18:47:50 +0400

    while loop with writer

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -5,13 +5,19 @@ import datetime
 
 
 
-start_date = datetime.date(2022,2,2)
-URL = "https://www.cbr-xml-daily.ru/archive/"+str(start_date).replace("-","/")+"/daily_json.js"
-html_page = requests.get(URL)
-json_page = html_page.json()
-usd_value = json_page["Valute"]["USD"]["Value"]
-data = [str(start_date)], [str(usd_value)]
-with open("data.csv", "w") as file:
-    writer = csv.writer(file)
-    writer.writerow(data)
-print(str(start_date)+", "+str(usd_value))
+current_date = datetime.date(2022,2,2)
+
+while(current_date != datetime.date(2022,4,20)):
+    URL = "https://www.cbr-xml-daily.ru/archive/"+str(current_date).replace("-","/")+"/daily_json.js"
+    html_page = requests.get(URL)
+    json_page = html_page.json()
+    if("error" in json_page):
+        current_date += datetime.timedelta(days=1)
+        continue
+    else:
+        usd_value = json_page["Valute"]["USD"]["Value"]
+        data = [str(current_date)], [str(usd_value)]
+        with open("data.csv", "a",newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(data)
+        current_date += datetime.timedelta(days=1)
