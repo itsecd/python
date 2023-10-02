9d0d6c65d62e5bc52653a5469eff6091d4c7d42e
Yana Yakovleva <Yakovlevaya11.06@gmail.com>
2023-10-03 00:10:49 +0400

    main

--- Lab1/lab1-var6.py
+++ Lab1/lab1-var6.py
@@ -1,58 +1,68 @@
 import os
 import requests
+import logging
 from bs4 import BeautifulSoup
 from typing import Dict, List
+
+#Directing the log entry to a file
+logging.basicConfig(filename = "py_log.log",filemode='w', level=logging.DEBUG)
+
 """
 function download_images Downloads images from list
 """
-def download_images(my_list:list)->int:
+def download_images(my_list:list,folder_name:str):
     try:
-     folder_name=os.mkdir(f'dataset/{os.getenv("FOLDER_TIGER")}')
+     os.mkdir(f'dataset\{folder_name}')
      num = 0
+     logging.info('Uploading images')
      for i in my_list:
-        while num!=300:
+        if num!=1000:
          response = requests.get(i)
-         with open(f"dataset/{folder_name}/{os.path.join(format(num).zfill(4))}.jpg", 'wb') as f:
+         with open(os.path.join("dataset",f"\{folder_name}",f"{format(num).zfill(4)}.jpg"), 'wb') as f:
             f.write(response.content)
          num += 1
-    except: 
-       print('No file by that name')
+        else:
+           break
+     logging.info(f'Images uploaded:{num}')
+    except Exception as e:  
+        logging.error('Error in download_images: ' + str(e))
 
 """
 function correct_images checks for duplicate links and removes them
 Returns a sorted list
 """
 
-def correct_images(my_list:list)->list:
-    print('total: ', len(my_list))
+def correct_images(my_list:list,folder_name:str)->list:
     sorted_list = list(set(my_list))
-    print('total: ', len(sorted_list))
-    download_images(sorted_list)
+    logging.info('the list is sorted')
+    download_images(sorted_list,folder_name)
 
 """
 The function searches for all src references and adds them to the list.
 Returns a list
 """
 
-def create_list(search:str)->list:
+def create_list(search:str,folder_name:str)->list:
+    logging.info('Create list')
     list = []
     count = 0
-    for pages in range(1,10):
-        for i in search:
-            while i!=pages
+    for pages in range(1,33):
+        url = f"https://www.yandex.ru/images/search?lr=51&p={pages}&rpt=image&{search}"
         response = requests.get(url).text
         soup = BeautifulSoup(response, 'lxml')
         images = soup.find_all('img')
         for image in images[1:]:
-            while count!=300:
-             count += 1
-             list.append("https:"+image['src'])
-    correct_images(list)
+            if count==1200:
+             break
+            count += 1
+            list.append("https:"+image['src'])
+    logging.info("list created,ready for sorted")
+    correct_images(list,folder_name)
 
 """
 Create a dataset folder in which we will store 
 folders with images and pass the url
 """
 os.mkdir('dataset')
-create_list(os.getenv("SEARCH_TIGER"))
-create_list(os.getenv("SEARCH_LEOPARD"))
+create_list(os.getenv("TEXT_TIGER"),os.getenv("FOLDER_TIGER"))
+create_list(os.getenv("TEXT_LEOPARD"),os.getenv("FOLDER_LEOPARD"))
