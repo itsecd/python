6236516dcde54b6c7a2e9a6f8171f3a799cbf9b5
Alina <ashavarina07@gmail.com>
2023-10-05 00:59:37 +0400

    removing constants and adding create_directory

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,17 +1,25 @@
 import os
-import shutil
+import os.path
 import requests
 from bs4 import BeautifulSoup
 
-def delete(folder:str):
-    shutil.rmtree(folder) 
-# удаляет текущую директорию и все поддиректории 
+HEADERS={"User-Agent": "Mozilla/5.0"}
+MAIN_FOLDER="dataset"
+FOLDER_CAT="cat"
+FOLDER_DOG="dog"
+url_cat="https://yandex.ru/images/search?text=cat.jpg"
+url_dog="https://yandex.ru/images/search?text=dog.jpg"
 
-URL_CAT = "https://yandex.ru/"
-URL_DOG = "https://yandex.ru/"
+def create_directory(folder: str)->str:
+    try:
+        if not os.path.exists(folder): #возвращает false, если путь не существует
+            os.makedirs(folder) #создает промежуточные каталоги по пути folder, если они не существуют
+    except: 
+        print("Folder don't create")
 
-HTML_CAT = requests.get(URL_CAT, headers={"User-Agent":"Mozilla/5.0"})
-HTML_DOG = requests.get(URL_DOG, headers={"User-Agent":"Mozilla/5.0"})
-
-SOUP_CAT = BeautifulSoup(HTML_CAT.text, 'lxml')
-SOUP_DOG = BeautifulSoup(HTML_DOG.text, 'lxml')
+def make_lists(url:str)->list:
+    list_url=[]
+    html = requests.get(url_cat, HEADERS)
+    soup = BeautifulSoup(html.text, "lxml")
+    print("List don't create") 
+ 
