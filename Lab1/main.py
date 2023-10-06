5834b35992c3e31795a1f11bbe7cc7d1f4ed1c69
Вероника Будюк <veronika.budyuk2004@gmail.com>
2023-10-07 01:12:49 +0400

    flexible download function for any classes

--- Lab1/main.py
+++ Lab1/main.py
@@ -3,79 +3,85 @@ import json
 import logging
 import requests
 from bs4 import BeautifulSoup
+from constans import HEADERS
 
 
 logging.basicConfig(level=logging.INFO)
 
 
-with open(os.path.join("Lab1", "fcc.json").replace("\\", "/"), "r") as fcc_file:
-    fcc = json.load(fcc_file)
-
-
 def create_directory(folder: str) -> str:
-    """The function takes the path to the folder and its name,
-    checks its presence.
+    """The function takes the path to the folder and it's name,
+    checks it's presence.
     In case of absence creates a folder.
     """
     try:
         if not os.path.exists(folder):
             os.makedirs(folder)
-    except:
-        logging.error("Folder don't create", exc_info=True)
+    except Exception as ex:
+        logging.error(f"Couldn't create folder: {ex.message}\n{ex.args}\n")
 
 
-def make_lists(url: str) -> list:
+def make_list(url: str) -> list:
     """The function accepts a link to a search query.
     Creates a list that contains links to each object
     from all specified request pages.
     """
     list_url = []
-    url_new = url[:-1]
     try:
-        for pages in range(fcc.get("PAGES")):
+        for pages in range(fcc["pages"]):
+            url_new = url[:-1]
             url_pages: str = f"{url_new}{pages}"
-            html = requests.get(url_pages, fcc.get("HEADERS"))
+            html = requests.get(url_pages, HEADERS)
             soup = BeautifulSoup(html.text, "lxml")
             flowers = soup.findAll("img")
             list_url += flowers
         return list_url
-    except:
-        logging.error("List don't create", exc_info=True)
+    except Exception as ex:
+        logging.error(f"List don't create: {ex.message}\n{ex.args}\n")
 
 
-def download(url_list: list, folder: str) -> str:
-    """The function accepts a list of links and a folder name.
-    Downloads all images from the list to a folder
+def download(
+    max_files: int,
+    classes: str,
+    url: str,
+    main_folder: str,
+) -> str:
+    """The function accepts number of files, classes (for splitting into folders), url and main-folder name.
+    Make a list with all images in classes, downloads from list this images
     and assigns them a unique number.
     """
     count = 0
     except_count = 0
-    for link in url_list:
-        if count > fcc.get("MAX_FILES"):
-            break
-        try:
-            src = link["src"]
-            print(src)
-            response = requests.get(src)
-            create_directory(
-                os.path.join(fcc.get("MAIN_FOLDER"), folder).replace("\\", "/"))
+    for c in classes:
+        url_list = make_list(url.replace("classes", c))
+        for link in url_list:
+            count_files = len(os.listdir(os.path.join(main_folder, c)))
+            if count_files > max_files:
+                count = 0
+                continue
             try:
-                with open(
-                        os.path.join(fcc.get("MAIN_FOLDER"), folder, f"{count:04}.jpg").replace("\\", "/"), "wb") as file:
-                    file.write(response.content)
-                    count += 1
-            except:
-                logging.error("Uncorrect path")
-        except:
-            except_count += 1
-    logging.info(f"Quantity download files={count}")
-    logging.info(f"Quantity uncorrect URL={except_count}")
+                src = link["src"]
+                print(src)
+                response = requests.get(src)
+                create_directory(os.path.join(
+                    main_folder, c).replace("\\", "/"))
+                try:
+                    with open(os.path.join(main_folder, c, f"{count:04}.jpg").replace("\\", "/"), "wb") as file:
+                        file.write(response.content)
+                        count += 1
+                except Exception as ex:
+                    logging.error(f"Uncorrect path: {ex.message}\n{ex.args}\n")
+            except Exception as ex:
+                except_count += 1
+                logging.error(
+                    f"Quantity uncorrect URL={except_count}:{src}\n")
+        logging.info(
+            f"Quantity downloaded files in {c} class is {count_files}")
 
 
 if __name__ == "__main__":
+    with open(os.path.join("Lab1", "fcc.json"), "r") as fcc_file:
+        fcc = json.load(fcc_file)
 
-    r = make_lists(fcc.get("url_rose"))
-    download(r, fcc.get("FOLDER_ROSE"))
-
-    t = make_lists(fcc.get("url_tulip"))
-    download(t, fcc.get("FOLDER_TULIP"))
+    download(fcc["max_files"], fcc["classes"],
+             fcc["search_url"], fcc["main_folder"])
