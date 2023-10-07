d8ecd57abe688c6f1aa80d3ff8b3943d586d3518
Darktezi <Darktezi@mail.ru>
2023-10-07 20:17:47 +0400

    making functions and some small changes

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -3,24 +3,39 @@ import requests
 import csv
 import datetime
 
+def create_or_clear_csv_file(filename):
+    with open(filename, "w", newline="") as file:
+        pass
 
-os.system(r'nul>data.csv')
-current_date = datetime.date(2022,2,2)
+def write_data_to_csv(filename, data):
+    with open(filename, "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(data)
 
-while(current_date != datetime.date(2022,2,25)):
-    URL = "https://www.cbr-xml-daily.ru/archive/"+str(current_date).replace("-","/")+"/daily_json.js"
+def get_json(date):
+    URL = "https://www.cbr-xml-daily.ru/archive/" + date.replace("-", "/") + "/daily_json.js"
     html_page = requests.get(URL)
+    if html_page.status_code != 200:
+        return {"error": "Page not found"}
     json_page = html_page.json()
-    if("error" in json_page):
-        with open("data.csv", "a",newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow([str(current_date),"Page not found"])
-        current_date += datetime.timedelta(days=1)
-        continue
-    else:
-        usd_value = json_page["Valute"]["USD"]["Value"]
-        data = [str(current_date), str(usd_value)]
-        with open("data.csv", "a",newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow(data)
-        current_date += datetime.timedelta(days=1)
+    return json_page
+
+def main():
+    current_date = datetime.date(2022, 2, 2)
+    end_date = datetime.date(2022, 2, 25)
+    while current_date != end_date:
+        date_str = str(current_date)
+        json_page = get_json(date_str)
+        if "error" in json_page:
+            write_data_to_csv("data.csv", [date_str, "Page not found"])
+            current_date += datetime.timedelta(days=1)
+            continue
+        else:
+            usd_value = json_page["Valute"]["USD"]["Value"]
+            data = [date_str, str(usd_value)]
+            write_data_to_csv("data.csv", data)
+            current_date += datetime.timedelta(days=1)
+
+if __name__ == "__main__":
+    create_or_clear_csv_file("data.csv")
+    main()
