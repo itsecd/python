ecfb9f58631f5c44f8ddd30a5dc9a9236e05c565
Yana Yakovleva <Yakovlevaya11.06@gmail.com>
2023-10-08 19:22:15 +0400

    function fixes,switching to bing

--- Lab1/lab1-var6.py
+++ Lab1/lab1-var6.py
@@ -1,70 +1,84 @@
 import os
 import requests
 import logging
+import string
 from bs4 import BeautifulSoup
+from settings import (
+    FOLDER_TIGER,
+    FOLDER_LEOPARD,
+    COUNT_FOR_LIST,
+    COUNT_FOR_DOWNLOADS,
+    SEARCH_TIGER,
+    SEARCH_LEOPARD,
+)
 from typing import Dict, List
 
+HEADERS = {
+   "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+   "Referer":"https://www.bing.com/"
+}
+logging.basicConfig(filename="py_log.log", filemode="w", level=logging.DEBUG)
 
-logging.basicConfig(filename = "py_log.log",filemode='w', level=logging.DEBUG)
 
 
-def download_images(my_list:list,folder_name:str)->int:
-    """Downloads images from list"""
+def create_folder(folder_name: str) -> str:
     try:
-     os.mkdir(f'dataset/{folder_name}')
-     num = 0
-     logging.info('Uploading images')
-     for i in my_list:
-        if num!=os.getenv("COUNT_FOR_DOWNLOADS"):
-         response = requests.get(i)
-         numbers=format(num).zfill(4)
-         with open(os.path.join("dataset",f"{folder_name}",f"{numbers}.jpg").replace("\\","/"), 'wb') as f:
-            f.write(response.content)
-         num += 1
-        else:
-           break
-     logging.info(f'Images uploaded:{num}')
-    except Exception as e:  
-        logging.error('Error in download_images: ' + str(e))
+        if not os.path.exists(f"dataset/{folder_name}"):
+            os.mkdir(f"dataset/{folder_name}")
+    except Exception as e:
+        logging.error(f"Error creating folder: {str(e)}")
+    logging.info("folder created")
 
 
-"""
-def correct_images(my_list:list,folder_name:str)->list:
-    #function correct_images checks for duplicate links and removes them
-    #Returns a sorted list
-
-    sorted_list = list(set(my_list))
-    logging.info('the list is sorted')
-    download_images(sorted_list,folder_name)
-"""
-
-
-def create_list(search:str,folder_name:str)->list:
-    """
-    The function searches for all src references and adds them to the list.
-    Returns a list
-    """
-    logging.info('Create list')
+def create_list(url: str) -> list:
     list = []
     count = 0
-    for pages in range(1,200):
-        url = f"https://www.bing.com/images/search?q={search}&go=%D0%9F%D0%BE%D0%B8%D1%81%D0%BA&qs=ds&form=QBIRMH&first={pages}"
-        response = requests.get(url).text
-        soup = BeautifulSoup(response, 'lxml')
-        images = soup.find_all('img', class_="mimg")
-        for image in images[:1]:
-           if count==os.getenv("COUNT_FOR_LIST"):
-             break
-           count += 1
-           list.append(image['src'])
-           print(list)
-    logging.info("list created,ready for sorted")
-    download_images(list,folder_name)
+    try:
+        for pages in range(1, 30):
+            if count >= int(COUNT_FOR_LIST):
+                break
+            url_pages: str = f"{url[:-1]}{pages}"
+            response = requests.get(url_pages, headers=HEADERS)
+            soup = BeautifulSoup(response.text, "lxml")
+            images = soup.find_all("img")
+            list+=images
+            count += 1
+        return list
+    except Exception as e:
+        logging.error(f"list not created: {str(e)}")
+    logging.info("img uploaded to list")
 
-"""
-Create a dataset folder in which we will store 
-folders with images and pass the url
-"""
-os.mkdir('dataset')
-create_list(os.getenv("TEXT_TIGER"),os.getenv("FOLDER_TIGER"))
-create_list(os.getenv("TEXT_LEOPARD"),os.getenv("FOLDER_LEOPARD"))
+
+def download_images(url: str, folder_name: str) -> str:
+    list=create_list(url)
+    logging.info("ready for download")
+    num=0
+    for img_tag in list:
+        if len(list) < int(COUNT_FOR_DOWNLOADS):
+            continue
+        try:
+            src = img_tag["src"]
+            tag=img_tag.find('rp')
+            if(tag!=-1):
+                list.remove(img_tag)
+                img_tag+=1
+                break
+            print(src)
+            response = requests.get(src)
+            numbers = format(num).zfill(4)
+            create_folder(folder_name)
+            try:
+                with open(os.path.join("dataset", f"{folder_name}", f"{numbers}.jpg"), "wb") as f:
+                    f.write(response.content)
+                    num += 1
+            except Exception as e:
+                    logging.error(f"Error creating file: {str(e)}")
+        except Exception as e:
+            logging.error(f"Error processing link:{str(e)}")
+    logging.info("Images downloaded")
+
+
+if __name__ == "__main__":
+    os.mkdir("dataset")
+    download_images(SEARCH_TIGER, FOLDER_TIGER)
+    download_images(SEARCH_LEOPARD, FOLDER_LEOPARD)
--- Lab1/settings.py
+++ Lab1/settings.py
@@ -9,12 +9,12 @@ load_dotenv(dotenv_path=env_path)
 
 
 FOLDER_TIGER = os.getenv("FOLDER_TIGER")
-TEXT_TIGER=os.getenv("TEXT_TIGER")
 
 FOLDER_LEOPARD = os.getenv("FOLDER_LEOPARD")
-TEXT_LEOPARD=os.getenv("TEXT_LEOPARD")
 
 COUNT_FOR_LIST=os.getenv("COUNT_FOR_LIST")
 COUNT_FOR_DOWNLOADS=os.getenv("COUNT_FOR_DOWNLOADS")
 
-PATH=os.getenv("Users\\Yana\\Documents\\python-v6\\Lab1dataset\\")
+SEARCH_TIGER=os.getenv("SEARCH_TIGER")
+SEARCH_LEOPARD=os.getenv("SEARCH_LEOPARD")
+
