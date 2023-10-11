bcb389918989373964f385f8f14b357ba7026e3c
kksseonn <kote13241@gmail.com>
2023-10-12 01:03:24 +0400

    added function to dowload images

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -7,3 +7,8 @@ def create_folder(folder_name):
     if not os.path.exist(folder_name):
         os.makedirs(folder_name)
 
+def dowload_image(url,folder_name,image_number):
+    response=requests.get(url)
+    file_name=f"{folder_name}/{str(image_number).zfill(4)}.jpg"
+    with open(file_name,"wb") as file:
+        file.write(response.content)
