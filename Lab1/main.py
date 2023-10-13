7fc24d7ad4e220456e79f60b3f3d4ec64cb8ccd3
KsushaK <ksushak1803@gmail.com>
2023-10-13 13:29:59 +0300

    review status,the name of the film

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,62 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-
-
-# Функция для получения страницы с рецензиями по ссылке
-def get_reviews_page(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    else:
-        print(f"Не удалось получить страницу: {url}")
-        return None
-
-# Функция для парсинга рецензий на странице
-def parse_reviews(page_content):
-    soup = BeautifulSoup(page_content, 'lxml')
-    reviews = soup.find_all('div', class_='discovery-tiles__wrap')
-    return reviews
+import re
 
 # Создаем папки для хранения рецензий
-os.makedirs('dataset/good', exist_ok=True)
-os.makedirs('dataset/bad', exist_ok=True)
+if not os.path.exists("dataset/good"):
+    os.makedirs("dataset/good")
+if not os.path.exists("dataset/bad"):
+    os.makedirs("dataset/bad")
 
-url = "https://www.rottentomatoes.com/m/the_equalizer_3/reviews?type=user"
-r = requests.get(url)
-soup = BeautifulSoup(r.text,"lxml")
-film_links = soup.find_all('div')
-# Счетчики для файлов
-good_count = 0
-bad_count = 0
-
-# Цикл по ссылкам на фильмы
-for film_link in film_links:
-    # Получаем страницу с рецензиями
-    page_content = get_reviews_page(film_link)
-    if page_content:
-        # Парсим рецензии
-        reviews = parse_reviews(page_content)
-        # Определяем категорию рецензии (по ссылке)
-        if "status/bad" in film_link:
-            category = "bad"
-        else:
-            category = "good"
-
-        # Сохраняем рецензии в файлы
-        for review in reviews:
-            # Получаем название фильма
-            film_title = review.find('span', class_='p--small').text.strip()
-            # Определяем номер файла и формируем имя файла
-            if category == "bad":
-                filename = f'dataset/bad/{str(bad_count).zfill(4)}.txt'
-                bad_count += 1
+good_reviews = 0
+bad_reviews = 0
+max_reviews_for_one_film = 300
+page_number = 1
+film_reviews = "https://www.rottentomatoes.com/m/the_equalizer_3/reviews?type=top_critics"
+while good_reviews < max_reviews_for_one_film or bad_reviews < max_reviews_for_one_film:
+        response = requests.get(film_reviews, params={"page": page_number})
+        soup = BeautifulSoup(response.text, "html.parser") 
+        match = re.search(r"/m/(.*?)/reviews", film_reviews)
+        movie_title = match.group(1)
+        print(movie_title)    
+        # Находим блоки с рецензиями 
+        review_blocks = soup.find_all('div',class_="review-row")
+        for review_block in review_blocks:
+            # Получаем текст рецензии
+            review_text = review_block.find('p', class_="review-text").get_text()
+            # Определяем, является ли рецензия "good" или "bad"
+            element = review_block.find('score-icon-critic-deprecated')
+            category=element["state"]
+            if category == "rotten":
+                category = "Bed"
+                print(review_text,"///////////",category)
             else:
-                filename = f'dataset/good/{str(good_count).zfill(4)}.txt'
-                good_count += 1
+                category = "Good"
+                print(review_text,"///////////",category)
+            # Создаем путь для сохранения файла
+            review_number = good_reviews + bad_reviews
+            file_path = f"dataset/{category}/{str(review_number).zfill(4)}.txt"
+            with open(file_path, "w", encoding="utf-8") as file:
+                file.write(f"Movie: {movie_title}\n")
+                file.write(review_text)
+                        
+            if category == "good":
+                good_reviews += 1
+            else:
+                bad_reviews += 1
+        page_number += 1    
 
-            # Сохраняем рецензию в файл
-            with open(filename, 'w', encoding='utf-8') as file:
-                file.write(film_title + '\n')
-                file.write(review.text)
-print("Рецензии сохранены в папках dataset/good и dataset/bad.")
+    
