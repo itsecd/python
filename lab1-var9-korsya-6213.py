cdd3409fa3cc3506c2258e78cfd69200052f3376
EvaKorsya <korsaeva24@gmail.com>
2023-10-14 14:49:49 +0400

    added getting text from review

--- lab1-var9-korsya-6213.py
+++ lab1-var9-korsya-6213.py
@@ -1,1 +1,23 @@
-print("kk1")
+from bs4 import BeautifulSoup
+import requests
+from fake_headers import Headers
+import time
+
+def get_html_source(url):
+    headers=Headers(browser='chrome',os='win',headers=True).generate()
+    source=requests.get(url,headers)
+    return source
+
+def soup_search(sourse):
+    soup=BeautifulSoup(sourse.text,'lxml')
+    response_text=soup.find('div',class_="l22dd3882").text
+    return response_text
+
+def main():
+    url="https://www.banki.ru/services/responses/bank/alfabank/?page=3&rate=2"
+    source=get_html_source(url)
+    response_text=soup_search(source)
+    print(response_text)
+
+if __name__=='__main__':
+    main()
