4ad711c93a74b03b15dba8915d4b677a8d329af4
aidenpirs007@mail.ru <aidenpirs007@mail.ru>
2023-10-16 14:12:39 +0400

    Fixed downloading function

--- Lab1/main.py
+++ Lab1/main.py
@@ -3,41 +3,48 @@ import logging
 import requests
 from bs4 import BeautifulSoup
 
+BASE_URL = "https://www.bing.com/images/search"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
-"""The function creates a folder if it does not exist"""
 def create_directory(directory: str) -> None:
+    """The function creates a folder if it does not exist"""
     try:
         if not os.path.exists(directory):
             os.makedirs(directory)
     except Exception as exc:
         logging.exception(f"Can't create folder: {exc.message}\n{exc.args}\n")
 
-def download_img(search_query, save_directory, num_images=1000):
-    create_directory(save_directory)
 
-    for start in range(1, num_images, 1):
-        
-        base_url = f"https://www.bing.com/images/search?q={search_query}&form=HDRSC2&first={start}"
-        try:
-            response_text = get_html_page(base_url)  # Получаем HTML-страницу
-            soup = BeautifulSoup(response_text, "html.parser")
-
-            img_tags = soup.find_all("img", class_="mimg")
-            for i, img_tag in enumerate(img_tags):
-                img_url = img_tag.get("src")
-                img_data = requests.get(img_url).content
-                img_filename = f"{i+start:04d}.jpg"
-                img_path = os.path.join(save_directory,img_filename)
-
-                with open(img_path, "wb") as img_file:
-                    img_file.write(img_data)
-
-                print(f"Скачано изображение {img_filename}")
-        except requests.exceptions.MissingSchema:
-         print(f"Проблема с URL: {base_url}")
-           # continue
-
-    print(f"Загружено {num_images} изображений класса {search_query}")
+def download_img(search_query: str, num_images: int) -> None:
+    """This function uploads images to the selected directory"""
+    create_directory("dataset")
+    count = 1
+    start = 0
+    while count < num_images:
+        base_url = f"{BASE_URL}?q={search_query}&form=HDRSC2&first={start}"
+        headers = {
+            "User-Agent": USER_AGENT,
+        }
+        response_text = requests.get(base_url, headers=headers)
+        soup = BeautifulSoup(response_text, "lxml")
+        img_tags = soup.find_all('img', {"src": True}, class_='mimg')
+        for img in img_tags:
+            img_url = img["src"]
+            if img_url:
+                try:
+                    img_data = requests.get(img_url headers=headers, timeout=10).content
+                    class_folder = f"dataset/{search_query}"
+                    create_directory(class_folder)
+                    with open(f"{class_folder}/{count:04}.jpg", "wb") as img_file:
+                        img_file.write(img_data)
+                        print(f"Downloaded image {img_data}")
+                    count += 1
+                    if count >= num_images:
+                        break
+                except Exception as e:
+                    logging.exception(f"Error downloading image: {e.message}\n{e.args}\n")
+        start += 1
+    print(f"Downloaded {count} изображений класса {search_query}")
 
 classes = ["polar_bear", "brown_bear"]
 num_images_for_class = 1000
