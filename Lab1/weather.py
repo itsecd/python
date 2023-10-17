595e9604f5d33ed7910c7eb146d3b0e79bee6c20
John Doe <johndoe@email.com>
2023-10-18 01:18:12 +0300

    commit#3

--- Lab1/weather.py
+++ Lab1/weather.py
@@ -2,8 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import csv
+from datetime import datetime
 
-url = 'https://www.gismeteo.ru/diary/4618/2023/10/'
+url = 'https://www.gismeteo.ru/diary/4618/'
+
+
+def get_url_with_current_date():
+    date = datetime.strptime(str(datetime.now().date()), "%Y-%m-%d")
+    return '/'.join([str(date.year),str(date.month)])
 
 
 def get_data(url):
@@ -23,17 +29,19 @@ def get_rows_of_weather_table(data):
     return soup.find_all("tr", {"align": "center"})
 
 
-def write_weather_csv():
+def write_weather_csv(url):
+    url = url+get_url_with_current_date()+'/'
     data = get_data(url)
     rows = get_rows_of_weather_table(data)
+
 
     header = ['Дата', 'Температура', 'Давление', 'Ветер']
     with open('weather.csv', 'w') as f:
         writer = csv.writer(f)
         writer.writerow(header)
         for i in rows:
-            writer.writerow([j for j in i.find_all('td') if j.get_text() != ""][:4])
-
+            writer.writerow([j.get_text() for j in i.find_all('td') if j.get_text() != ""][:4])
+            print([j.get_text() for j in i.find_all('td') if j.get_text() != ""][:4])
 
 if __name__ == '__main__':
-    write_weather_csv()
+    write_weather_csv(url)
