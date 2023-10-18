a4ae6a3f029c7982e15a573b844d35f99c73cf30
Sanechek600 <dassander@mail.ru>
2023-10-18 16:44:58 +0400

    Added argparse, circling function, docstrings etc.

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,40 +1,45 @@
-from bs4 import BeautifulSoup
 import requests
 import os
 import re
-import codecs
-import chardet
 import logging
+import argparse
+from time import sleep
+from bs4 import BeautifulSoup
+import chardet
 
 
-#Блок работы с запросом и кодом страницы
+HEADERS = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        }
+
+logging.basicConfig(level=logging.INFO)
+
+
 def get_page(URL: str) -> str:
-    html_page = requests.get(URL, headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"}, timeout=100)
+    '''Takes in URL of a page, returns a soup for further parsing'''
+    sleep(5)
+    html_page = requests.get(URL, headers=HEADERS)
     encoding = chardet.detect(html_page.content)['encoding'] #Определяем кодировку страницы
     decoded_html_page = html_page.content.decode(encoding) #Декодируем страницу. Иначе кириллические символы вызовут UnicodeError
     soup = BeautifulSoup(decoded_html_page, features="html.parser")
     return soup
 
 
-#Блок работы с директориями. 
 def create_dataset(name: str) -> None:
-    if not os.path.exists(f"{name}"):
-        os.mkdir(name)
-        for i in range(0, 6):
-            os.mkdir(f"{name}/{i}")
+    '''Takes in a name of dataset directory, creates the directory if it is missing'''
+    if not os.path.exists(name):
+
+        try:
+            os.mkdir(name)
+            for i in range(0, 6):
+                os.mkdir(f"{name}/{i}")
+        except Exception as exc:
+            logging.exception(f"Can't create folder: {exc.message}\n{exc.args}\n")
 
 
-#Блок парсинга 
-def parse_soup(soup: str, dataset_name: str) -> dict[str, int]:
+def parse_soup(soup: str, dataset_name: str, card_nums: dict[str, int]) -> dict[str, int]:
+    '''Parses a given soup string and saves review entries into dataset, returns the size of every subdirectory of dataset'''
     cards = soup.find_all("article", class_="review-card lenta__item") #Находим все рецензии
-    card_nums = {
-        '0': 0,
-        '1': 0,
-        '2': 0,
-        '3': 0,
-        '4': 0,
-        '5': 0,
-    }
 
     for card in cards:
 
@@ -57,20 +62,18 @@ def parse_soup(soup: str, dataset_name: str) -> dict[str, int]:
         for p in full_text_p[1:]: #Здесь 0-й элемент - повтор первого параграфа, поэтому 0-й отбрасываем
             full_text += (p.text + "\n")
 
-        with open(f"{dataset_name}/" + rating + "/" + str(card_nums[rating]).zfill(4), "w", encoding="utf-8") as f:
+        with open(os.path.join(dataset_name, rating, f"{str(card_nums[rating]).zfill(4)}.txt"), "w", encoding="utf-8") as f:
             f.write(title + full_text)
-            print(f"Update: {rating} -- {card_nums[rating]}")
         card_nums[rating] += 1
     
     return card_nums
-        
 
-URL = "https://www.livelib.ru/reviews/~1#reviews"
 
-os.chdir("Lab1")
-create_dataset("dataset")
-
-card_nums = {
+def circle_pages(URL: str, dataset_name: str, target_size: int) -> None:
+    '''Takes in a URL, a name of dataset and a target size of the dataset'''
+    i = 1
+    dataset_size = 0
+    card_nums = {
         '0': 0,
         '1': 0,
         '2': 0,
@@ -78,19 +81,23 @@ card_nums = {
         '4': 0,
         '5': 0,
     }
-i = 1;
-while card_nums["4"] < 999 and card_nums["5"] < 999:
-    soup = get_page(f"https://www.livelib.ru/reviews/~{i}#reviews")
-    card_nums = parse_soup(soup, "dataset")
-    i+=1
+    while dataset_size < target_size: 
+        soup = get_page(URL)
+        print(URL)
+        card_nums = parse_soup(soup, dataset_name, card_nums)
+        dataset_size = card_nums["0"] + card_nums["1"] + card_nums["2"] + card_nums["3"] + card_nums["4"] + card_nums["5"]
+        i += 1
+        URL = URL.replace(f"reviews/~{i-1}", f"reviews/~{i}")
 
 
+if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser(description='Save review entries into the dataset')
+    parser.add_argument('--url', type=str, default="https://www.livelib.ru/reviews/~1#reviews", help='URL of the site to scrap')
+    parser.add_argument('--ds', type=str, default="dataset", help='Name of the dataset directory')
+    parser.add_argument('--size', type=int, default=1000, help='Target size of the dataset')
+    args = parser.parse_args()
 
-
-
-
-
-
-
-
+    create_dataset(args.ds)
+    circle_pages(args.url, args.ds, args.size)
+    
