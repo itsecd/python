1249b36d051a33e7a07c16527f88a21620765cf3
Viktor <shehmatov.vitek@gmail.com>
2023-10-19 09:05:07 +0300

    search "unigue" images

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,32 +1,24 @@
-import os
-import requests
-from bs4 import Beautifulsoup
-
+from ImageScrapper import Image_Scrapper as IS
 
 MAIN_DIR = "dataset"
 POLAR_BEAR_DIR = "polar_beer"
 BROWN_BEAR_DIR = "brown_beer"
 HEADER = {"User-Agent":"Mozilla/5.0"}
-url_polar_bear = "https://yandex.ru/images/search?text=polar%20bear"
-url_brown_bear = "https://yandex.ru/images/search?text=brown%20bear"
+url_polar_bear = "https://www.bing.com/images/search?q=polar_bear.jpg&first=1"
+url_brown_bear = "https://www.bing.com/images/search?q=brown_bear.jpg&&first=1"
 
-class Image_Scrapper():
-    def __init__(self, _dir : str, _url : str, _header:dict[str,str]):
-        self.dir = _dir
-        self.url = _url
-        self.header = _header
-    def create_dir():
-        pass
-    def create_list():
-        pass
-    def download():
-        pass
 
 if __name__ == "__main__":
-    pb_scrap = Image_Scrapper(POLAR_BEAR_DIR, url_polar_bear, HEADER)
-    bb_scrap = Image_Scrapper(BROWN_BEAR_DIR, url_brown_bear, HEADER) 
-    pb_scrap.download()   
-    bb_scrap.download()    
+    pb_scrap = IS(POLAR_BEAR_DIR, MAIN_DIR, url_polar_bear, HEADER)
+    bb_scrap = IS(BROWN_BEAR_DIR, MAIN_DIR, url_brown_bear, HEADER) 
+
+    pb_scrap.get_pages_imgs(1050, 50)
+    pb_scrap.download() 
+
+    bb_scrap.get_pages_imgs(1050, 50)
+    bb_scrap.download()   
+     
+ 
 
 
 
