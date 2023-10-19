e4bf00e9fdc0651b33f2bd573956f74a9d5ef94f
Борис Шестериков <boris20033@gmail.com>
2023-10-20 01:44:25 +0400

    Make def for make new fold

--- Lab2/script_3.py
+++ Lab2/script_3.py
@@ -1,27 +1,29 @@
-import csv
 import json
 import os
-import time
 import random
 import logging
 import shutil
-from script_1 import create_csv
+from script_1 import create_csv, write_in_csv
 
-def make_new_folder(name_csv : str, img_class : str, directory : str) -> None:
+def make_new_fold(name_csv : str, img_class : str, directory : str) -> None:
     try:
         create_csv(name_csv)
         number_of_img = len(os.listdir(os.path.join(directory, img_class)))
         for img in range(number_of_img):
-            shutil.copyfile(os.path.join(directory, f"{random.randint(0, 10000)}.jpg"), os.path.join(directory, f"{img_class}_{img:04}.jpg"))
+            _random = random.randint(0, 10000)
+            shutil.copyfile(os.path.join(directory, img_class, f"{img:04}.jpg"), os.path.join(directory, f"{_random}.jpg"))
             # os.remove(os.path.join(directory, img_class, f"{img:04}.jpg"))
 
-            # write_in_csv(name_csv, img_class, os.path.join(directory, f"{img_class}_{img:04}.jpg"))
+            write_in_csv(name_csv, img_class, os.path.join(directory, f"{_random}.jpg"))
             
     except Exception as ex:
         logging.error(f"Error of copy img : {img} | {ex}")
+
 
 if __name__ == '__main__':
     with open(os.path.join("Lab1", "input_data.json"), 'r') as fjson:
         fj = json.load(fjson)
 
     logging.basicConfig(level=logging.INFO)
+
+    make_new_fold("Lab2/dataset_random", fj["object"], fj["main_folder"])
