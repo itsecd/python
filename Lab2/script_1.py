18ecf6609813368dd31d72d90442d1f034eea642
Борис Шестериков <boris20033@gmail.com>
2023-10-19 22:12:15 +0400

    Create script_1

--- Lab2/script_1.py
+++ Lab2/script_1.py
@@ -1,4 +1,5 @@
 import csv
+import json
 import os
 import logging
 
@@ -11,5 +12,25 @@ def create_csv(name_csv : str) -> None:
     except Exception as ex:
         logging.error(f"Error create csv file: {ex}")
 
+def write_in_file(name_csv : str, img_class : str, directory : str) -> None:
+    try:
+        create_csv(name_csv)
+        for img in range(len(os.listdir(os.path.join(directory, img_class)))):
+            row = [
+                os.path.abspath(os.path.join(directory, img_class, f"{img:04}.jpg")),
+                os.path.join(directory, img_class, f"{img:04}.jpg"),
+                img_class
+            ]
+            with open(f"{name_csv}.csv", "a") as file:
+                writer = csv.writer(file)
+                writer.writerow(row)
+    except Exception as ex:
+        logging.error(f"Error of add to csv file : {img} | {ex}")
+
+
 if __name__ == '__main__':
+    with open(os.path.join("Lab1", "input_data.json"), 'r') as fjson:
+        fj = json.load(fjson)
+
     logging.basicConfig(level=logging.INFO)
+    write_in_file("dataset", fj["object"], os.path.join("Lab1", fj["main_folder"]))
