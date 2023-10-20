08a1e247c9e75b619a1ddfe337f9adbbc2d82b62
Quyntrd <daniil.terebilkin@gmail.com>
2023-10-20 15:18:06 +0400

    changed some names

--- Lab1/main.py
+++ Lab1/main.py
@@ -6,7 +6,7 @@ import argparse
 from bs4 import BeautifulSoup
 
 BASE_URL = "https://otzovik.com/reviews"
-HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+HEADERS = {"User-Agent":"Mozilla/5.0"}
 
 
 logging.basicConfig(level=logging.INFO)
