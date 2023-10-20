94395bc0139946c6eaea7ace2451b3f9049056a1
Борис Шестериков <boris20033@gmail.com>
2023-10-20 12:23:07 +0400

    Add ability to download many objects

--- Lab1/main.py
+++ Lab1/main.py
@@ -23,39 +23,42 @@ def create_folder(folder: str) -> None:
         logging.error(f"{err}", exc_info=True)
 
 
-def download(list_images : list, folder : str, count_images : int) -> None:
+def download(objects : list, folder : str, count_images : int) -> None:
     """
     Download images
 
     Download images from list
     Parameters
     ----------
-    list_images : list
-        Список тегов с картинками
+    object : list
+        Список объектов для скачивания
     folder : str
-        Путь для скачивания
+        Путь гланой папки для скачивания
     count : int
         Кол-во картинок для скачивания
     """
-    count = 0
-    exec_count = 0
-    for flower_url in list_images:
-        if count >= count_images : break
-        try:
-            src=flower_url['src']
-            response = requests.get(src)
-            create_folder(folder)
-            with open(os.path.join(folder, f"{count:04}.jpg").replace("\\", "/"), "wb") as file:
-                file.write(response.content)
-                count+=1
-        except Exception as err:
-            exec_count+=1
-            logging.warning(f"Error image_url {count+exec_count+1}. {err}")
+    
+    for object in objects:
+        count = 0
+        exec_count = 0
+        list_images = make_list(object, URL, count_images)
+        for flower_url in list_images:
+            if count >= count_images : break
+            try:
+                src=flower_url['src']
+                response = requests.get(src)
+                create_folder(os.path.join(folder, object))
+                with open(os.path.join(folder, object, f"{count:04}.jpg").replace("\\", "/"), "wb") as file:
+                    file.write(response.content)
+                    count+=1
+            except Exception as err:
+                exec_count+=1
+                logging.warning(f"Error image_url {count+exec_count+1}. {err}")
     logging.info(f"Successful download - {count}")
     logging.info(f"Unsuccessful download - {exec_count}")
 
 
-def make_list(url : str, count : int) -> list:
+def make_list(object : str, url : str, count : int) -> list:
     """
     Make list of images tags
 
@@ -72,7 +75,7 @@ def make_list(url : str, count : int) -> list:
         Список с тегами картинок 
     """
     list_img = []
-    url = url.replace("object", OBJECT)
+    url = url.replace("object", object)
     new_url = url[:-1]
     try:
         for page in range(1, count):
@@ -100,7 +103,6 @@ if __name__ == "__main__":
     HEADERS = fj["headers"]
     COUNT = fj["counter"]
     MAIN_FOLDER =  fj["main_folder"]
-    OBJECT = fj["object"]
+    OBJECTS = fj["objects"]
 
-    images = make_list(URL, COUNT)
-    download(images, os.path.join(MAIN_FOLDER, OBJECT).replace("\\", "/"), COUNT)
+    download(OBJECTS, MAIN_FOLDER, COUNT)
