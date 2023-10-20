87cd693df727aab629c4159169387e2ada3367f2
Борис Шестериков <boris20033@gmail.com>
2023-10-20 14:36:37 +0400

    Fix path

--- Lab2/get_way.py
+++ Lab2/get_way.py
@@ -1,10 +1,10 @@
 def get_path_together(object : str, number : int) -> str:
-    return f"dataset/dataset_together/{object}_{number:04}.jpg"
+    return f"Lab1/dataset/dataset_together/{object}_{number:04}.jpg"
 
 
 def get_path_random(object : str, number : int) -> str:
-    return f"dataset/dataset_random/{number:04}.jpg"
+    return f"Lab1/dataset/dataset_random/{number:04}.jpg"
 
 
 def get_path_normal(object : str, number : int) -> str:
-    return f"dataset/{object}/{number:04}.jpg"
+    return f"Lab1/dataset/{object}/{number:04}.jpg"
