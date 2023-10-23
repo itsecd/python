d927adc3d9a1356067f37c0134184483ec77a161
KsushaK <ksushak1803@gmail.com>
2023-10-23 17:14:47 +0300

    you can control max review(argparse)

--- Lab1/lab_1_var_8.py
+++ Lab1/lab_1_var_8.py
@@ -5,7 +5,7 @@ from time import sleep
 import logging
 import random
 from fake_useragent import UserAgent
-
+import argparse
 
 def generate_random_user_agent():
     ua = UserAgent()
@@ -13,21 +13,26 @@ def generate_random_user_agent():
 
 logging.basicConfig(level=logging.INFO)
 
+parser = argparse.ArgumentParser(description='Сбор рецензий с КиноПоиск')
+parser.add_argument('--max_reviews', type=int, default=1000, help='Максимальное количество рецензий (good и bad) для сбора')
+args = parser.parse_args()
+
 try:
-    if not os.path.exists("dataset/good"):
-        os.makedirs("dataset/good")
-    if not os.path.exists("dataset/bad"):
-        os.makedirs("dataset/bad")
+    for folder_name in ["good", "bad"]:
+        folder_path = os.path.join("dataset", folder_name)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
 except Exception:
     logging.exception(f"Ошибка при создании папки")
 
-def get_page(page):
-    url = "https://www.kinopoisk.ru/film/435/reviews/ord/rating/status/all/perpage/10/page/" + str(page) + "/"
+def get_page(page) -> BeautifulSoup:
+    base_url = "https://www.kinopoisk.ru/film/435/reviews/ord/rating/status/all/perpage/10/page/"
+    url = f"{base_url}{page}/"
     try:
-        sleep_time = random.uniform(1, 3)
+        sleep_time = random.uniform(5, 7)
         sleep(sleep_time)
         headers = {
-            "User-Agent": generate_random_user_agent()  # Используем случайный User-Agent
+            "User-Agent": generate_random_user_agent()
         }
         response = requests.get(url, headers=headers)
         response.raise_for_status()
@@ -42,14 +47,12 @@ def get_page(page):
 
 def process_review(review_block, good_reviews, bad_reviews):
     try:
-        # Получаем текст рецензии
         text_element = review_block.find('div', class_="brand_words")
         if text_element is not None:
             review_text = text_element.get_text()
         else:
             review_text = "Текст рецензии не найден"
             print(review_text)
-        # Определяем, является ли рецензия "good" или "bad"
         element = review_block.find('ul', class_='voter')
         if element:
             li_elements = element.find_all('li')
@@ -61,51 +64,45 @@ def process_review(review_block, good_reviews, bad_reviews):
                 else:
                     category = "good"
 
-                # Создаем уникальный идентификатор в виде числа от 0001 до 0999
                 if category == "good":
-                    good_reviews = len(os.listdir("dataset/good"))
-                    unique_id = str(good_reviews + 1).zfill(4)
+                    folder_name = "good"
+                    good_reviews_count = len(os.listdir(os.path.join("dataset", folder_name)))
+                    unique_id = str(good_reviews_count + 1).zfill(4)
                 else:
-                    bad_reviews = len(os.listdir("dataset/bad"))
-                    unique_id = str(bad_reviews + 1).zfill(4)
-
-                # Создаем путь к файлу, включая папку с категорией
+                    folder_name = "bad"
+                    bad_reviews_count = len(os.listdir(os.path.join("dataset", folder_name)))
+                    unique_id = str(bad_reviews_count + 1).zfill(4)
                 file_name = f"{unique_id}_{category}.txt"
-                file_path = os.path.join("dataset", category, file_name)
-                film_title_ = soup.find('div',class_="breadcrumbs__sub")
-                if film_title_ is not None:
-                    film_title = film_title_.get_text()
+                file_path = os.path.join("dataset", folder_name, file_name)
+                film_title_element = soup.find('div', class_="breadcrumbs__sub")
+                if film_title_element is not None:
+                    film_title = film_title_element.get_text()
                 else:
                     film_title = "Название фильма не найдено"
                     print(film_title)
-
-                # Записываем информацию в файл
                 with open(file_path, "w", encoding="utf-8") as file:
                     file.write(f"Film: {film_title}\n")
                     file.write(review_text)
     except Exception:
         print(f"Ошибка при обработке рецензии")
-
     return good_reviews, bad_reviews
 
 good_reviews = 0
 bad_reviews = 0
-max_reviews_for_one_film = 10
-page_number = 2
-
+max_reviews_for_one_film = args.max_reviews
+page_number = 1
 while good_reviews < max_reviews_for_one_film or bad_reviews < max_reviews_for_one_film:
     soup = get_page(page_number)
     if soup:
         review = soup.find('div', class_="clear_all")
         if review is not None:
-            review_blocks=review.find_all('div',class_="reviewItem userReview")
+            review_blocks = review.find_all('div', class_="reviewItem userReview")
             for review_block in review_blocks:
                 good_reviews, bad_reviews = process_review(review_block, good_reviews, bad_reviews)
             page_number += 1
         else:
             print("Ошибка при отправке запроса на страницу.")
 
-
-print(f"Собрано {good_reviews+1} хороших рецензий и {bad_reviews+1} плохих рецензий.")    
+print(f"Собрано {good_reviews + 1} хороших рецензий и {bad_reviews + 1} плохих рецензий.")
 
     
