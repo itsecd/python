398fecef71601611f74e307314f2f6862c7776dc
Alina <ashavarina07@gmail.com>
2023-11-11 15:42:30 +0400

    versia 1

--- Lab1/main.py
+++ Lab1/main.py
@@ -6,29 +6,32 @@ from bs4 import BeautifulSoup
 
 logging.basicConfig(level=logging.INFO)
 
-def create_directory(folder: str) -> str: 
+def create_directory(folder: str) -> str:
     '''принимает путь к папке и ее имя'''
     try:
-        if not os.path.exists(folder):  
-            os.makedirs(folder) 
+        if not os.path.exists(folder):
+            os.makedirs(folder)
     except Exception as ex:
-        logging.error(f"Error in create_directory") 
+        logging.error(f"Error in create_directory")
 
-def make_list(url: str) -> list: 
+
+def make_list(url: str) -> list:
     '''принимает ссылку на запрос'''
     list_url = []
     try:
         for pages in range(main["pages"]):
             url_new = url[:-1]
             url_pages: str = f"{url_new}{pages}"
-            responce = requests.get(url_pages, main['headers']).text # делаем запрос и получаем html
-            soup = BeautifulSoup(responce, "lxml") # используем парсер lxml
-            animals= soup.findAll("img")
+            # делаем запрос и получаем html
+            responce = requests.get(url_pages, main['headers']).text
+            soup = BeautifulSoup(responce, "lxml")  # используем парсер lxml
+            animals = soup.findAll("img")
             list_url += animals
         return list_url
     except Exception as ex:
         logging.error(f"Error in make_list")
-        
+
+
 def download(
     folder: str,
     search: str,
@@ -39,10 +42,10 @@ def download(
     count = 0
     incorrect_url = 0
     for cd in search:
-        url_list = make_list(url.replace("search", cd)) 
+        url_list = make_list(url.replace("search", cd))
         for exile in url_list:
-            total_files = len(os.listdir(os.path.join(folder, cd))) 
-            if total_files > max_files: 
+            total_files = len(os.listdir(os.path.join(folder, cd)))
+            if total_files > max_files:
                 continue
             try:
                 src = exile["src"]
@@ -59,8 +62,9 @@ def download(
                 logging.error(f"Total incorrect URL: {incorrect_url}")
     logging.info(f"Completoin of data loading")
 
+
 if __name__ == "__main__":
-    with open(os.path.join("Lab1", "main.json"), "r") as main_file: 
+    with open(os.path.join("Lab1", "main.json"), "r") as main_file:
         main = json.load(main_file)
 
-    download(main["folder"], main["search"], main["url"], main["max_files"])
+    download(main["folder"], main["search"], main["url"], main["max_file"])
