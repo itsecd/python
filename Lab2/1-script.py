b1d8c04faf091659c9e291e1c0e90a5df442c26b
EvaKorsya <korsaeva24@gmail.com>
2023-11-12 12:34:39 +0400

    added docstrings,type-hints for 1-script

--- Lab2/1-script.py
+++ Lab2/1-script.py
@@ -1,20 +1,39 @@
 import csv
 import os
-#"C:\Users\korsy\OneDrive\Рабочий стол\pythonlabs\Lab2\dataset\1\0001.txt"
-#dataset\1\0001.txt
-#1
-rewievs=[[]]
-for star in range(1,6):
-    dir='dataset'
-    directory = os.path.join(dir, f"{star}")
-    files = [file for file in os.listdir(directory) if os.path.isfile(f'{directory}/{file}')]
-    cnt_files=len(files)
-    for file in range(1,cnt_files+1):
-        relative_path=os.path.join(directory, f"{str(file).zfill(4)}.txt")
-        absolute_path = os.path.abspath(relative_path)
-        rewievs.append([relative_path,absolute_path,star])
-filename='rewievs'
-with open(filename, "w", newline="") as file:
-    writer = csv.writer(file)
-    writer.writerows(rewievs)
 
+
+def get_annotation(dir='dataset') -> list[list[str, str, int]]:
+    """
+    the function creates list of lists consisting of three elements:
+    relative path, absolute path and class label for each file.
+    ----------
+    dir : str
+    """
+    rewievs = [[]]
+    for star in range(1, 6):
+        directory = os.path.join(dir, f"{star}")
+        files = [file for file in os.listdir(
+            directory) if os.path.isfile(f'{directory}/{file}')]
+        cnt_files = len(files)
+        for file in range(1, cnt_files + 1):
+            relative_path = os.path.join(
+                directory, f"{str(file).zfill(4)}.txt")
+            absolute_path = os.path.abspath(relative_path)
+            rewievs.append([relative_path, absolute_path, star])
+    return rewievs
+
+
+def write_csv(path='rewievs') -> None:
+    """
+    the function writes list elements to a csv file.
+    ----------
+    path : str
+    """
+    rewievs = get_annotation()
+    with open(path, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(rewievs)
+
+
+if __name__ == '__main__':
+    write_csv()
