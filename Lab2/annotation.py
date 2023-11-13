c634d8510bfb4b286e396f5b8b9574b0074d61ec
EvaKorsya <korsaeva24@gmail.com>
2023-11-13 18:46:38 +0400

    fixed get_annotation default params

--- Lab2/annotation.py
+++ Lab2/annotation.py
@@ -27,7 +27,7 @@ def makedir(path: str) -> None:
             logging.error(f"{err}", exc_info=True)
 
 
-def get_annotation(directory='dataset', new_dir='new_dataset', label_annotation=AnnotationLabel.DEFAULT) -> Generator[
+def get_annotation(directory='dataset', new_dir='new_dataset', label_annotation=0) -> Generator[
     list, None, None]:
     """
     create list of lists consisting of three elements:
@@ -55,32 +55,9 @@ def get_annotation(directory='dataset', new_dir='new_dataset', label_annotation=
                     raise Exception("Incorrect mode")
             absolute_path = os.path.abspath(relative_path)
             yield [absolute_path, relative_path, star]
-    # if (label_annotation != AnnotationLabel.DEFAULT):
-    #     makedir(new_dir)
-    # for star in range(1, 6):
-    #     dir = os.path.join(dir, f'{star}')
-    #     files = [file for file in os.listdir(dir) if os.path.isfile(f'{dir}/{file}')]
-    #     for file in files:
-    #         match AnnotationLabel(label_annotation):
-    #             case AnnotationLabel.DEFAULT:
-    #                 relative_path = os.path.join(dir, f'{str(file).zfill(4)}.txt')
-    #             case AnnotationLabel.NEWDIR:
-    #                 shutil.copy(os.path.join(dir, file), os.path.join(new_dir, f'{star}_{file}'))
-    #                 relative_path = os.path.join(new_dir, f'{file}')
-    #             case AnnotationLabel.RAND:
-    #                 print(dir)
-    #                 path_to_file = f'{str(random.randrange(10000)).zfill(4)}.txt'
-    #                 while os.path.isfile(os.path.join(new_dir, path_to_file)):  # replace number, if file exists
-    #                     path_to_file = f'{str(random.randrange(10000)).zfill(4)}.txt'
-    #                 shutil.copy(os.path.join(dir, file), os.path.join(new_dir, path_to_file))
-    #                 relative_path = os.path.join(new_dir, path_to_file)
-    #             case _:
-    #                 raise Exception("Incorrect mode")
-    #         absolute_path = os.path.abspath(relative_path)
-    #         yield [absolute_path, relative_path, star]
+    
 
-
-def write_csv(path_to_csv='reviews.csv', label_of_annotation=AnnotationLabel.DEFAULT, new_dir='new_dataset',
+def write_csv(path_to_csv='reviews.csv', label_of_annotation=0, new_dir='new_dataset',
               old_dir='dataset') -> str:
     """
     write list of elements to a csv file.
