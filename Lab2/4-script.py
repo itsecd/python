26f2c317c7469e55e8a53e968e56845551afc23c
EvaKorsya <korsaeva24@gmail.com>
2023-11-13 11:17:42 +0400

    5-script

--- Lab2/4-script.py
+++ Lab2/4-script.py
@@ -1,17 +1,28 @@
-import os
 import csv
-import sys
 
-def get_next(class_label:int, csv_path='reviews.csv'):
+
+def get_path_of_next(class_label:int,ind:int,csv_path='reviews.csv'):
+    """
+    the function returns the path of the element next after the element
+    whose index is passed.
+    class_label : int
+    ind : int
+    csv_path : str
+    """
     with open(csv_path, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        read=[]
-        for row in reader:
-            read.append(row)
-        print(len(read))
+        files=[]
+        for row in csv.reader(csvfile, delimiter=','):
+            if (class_label==int(row[-1])):
+                files.append(row)
+        cnt=0
+        for file in files:
+            if cnt!=ind:
+                cnt+=1
+            else:
+                return file[0] 
+        return None
         
              
 
-
 if __name__ == '__main__':
-    get_next(1)
+    print(get_path_of_next(1,1))
---
+++ Lab2/5-script.py
@@ -0,0 +1,31 @@
+import os
+import csv
+
+
+class RevIterator:
+    def __init__(self,path_to_csv:str,class_label:int) -> None:
+        self.class_label=class_label
+        self.path_to_csv=path_to_csv
+        self.reviews=[]
+        self.counter=0
+
+        with open(path_to_csv, newline='') as csvfile:
+            for row in csv.reader(csvfile, delimiter=','):
+                if (class_label==int(row[-1])):
+                    self.reviews.append(row)
+
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self) -> str:
+        if self.counter < len(self.reviews):
+            self.counter += 1
+            return self.reviews[self.counter-1][0]
+        else:
+            raise StopIteration
+        
+if __name__ == "__main__":
+    iter = RevIterator('reviews.csv',2)
+    for i in iter:
+        print(i)
