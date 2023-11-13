6f93af500a9b3e5c7c8465749e9907be0255273e
Yana Yakovleva <Yakovlevaya11.06@gmail.com>
2023-11-13 22:26:31 +0400

    done task 3,4

--- Lab2/copy_dataset.py
+++ Lab2/copy_dataset.py
@@ -7,17 +7,17 @@ import csv_
 logging.basicConfig(filename="Lab2\\py_log1.log", filemode="a", level=logging.INFO)
 
 def copy_dataset(old_dir:str, tags:str, new_dir:str, name_csv:str) -> None:
-    """Function copy images from tags folders to the dataset"""
+    """Function for copying images from tags folders to the dataset"""
     try:
         img_list=[]
         for tag in tags:
             count_files=len(os.listdir(os.path.join(old_dir,tag)))
-            for img in range(count_files):
-                b=os.path.abspath(os.path.join(old_dir,tag,f"{img:04}.jpg"))
-                a=os.path.abspath(os.path.join(new_dir,f"{tag}_{img:04}.jpg"))
+            for i in range(count_files):
+                b=os.path.abspath(os.path.join(old_dir,tag,f"{i:04}.jpg"))
+                a=os.path.abspath(os.path.join(new_dir,f"{tag}_{i:04}.jpg"))
                 shutil.copy(b,a)
-                l=[[a,os.path.realpath(a),tag]]
-                img_list+=l
+                img=[[a,os.path.realpath(a),tag]]
+                img_list+=img
         csv_.write_scv(name_csv,img_list)
         logging.info('copy file csv done')
     except Exception as e:
