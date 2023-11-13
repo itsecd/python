b77aa29fcd027edf7b6994095500d723dea8c1b2
Prodigysy <miracle.owsyannikov@yandex.ru>
2023-11-13 23:43:01 +0400

    optimazed code

--- Lab2/annotation's.py
+++ Lab2/annotation's.py
@@ -1,5 +1,5 @@
-
 import os
+from pathlib import Path
 import csv
 import shutil
 import random
@@ -11,16 +11,18 @@ logger = logging.getLogger(__name__)
 def create_annotation_file(dataset_directory: str, output_file: str, include_relative_path: bool = False) -> None:
     """Create annotation file for the dataset."""
     try:
+        paths = []
+        for root, dirs, files in os.walk(dataset_directory):
+            for file in files:
+                abs_path = Path(root) / file
+                rel_path = abs_path.relative_to(dataset_directory) if include_relative_path else None
+                class_label = Path(root).name
+                paths.append((abs_path, rel_path, class_label))
+
         with open(output_file, 'w', newline='') as csvfile:
             csvwriter = csv.writer(csvfile)
             csvwriter.writerow(['Absolute Path', 'Relative Path', 'Class Label'])
-
-            for root, dirs, files in os.walk(dataset_directory):
-                for file in files:
-                    abs_path = os.path.join(root, file)
-                    rel_path = os.path.relpath(abs_path, start=dataset_directory) if include_relative_path else None
-                    class_label = os.path.basename(root)
-                    csvwriter.writerow([abs_path, rel_path, class_label])
+            csvwriter.writerows(paths)
 
         logger.info(f"Annotation file '{output_file}' created successfully.")
     except Exception as e:
@@ -28,23 +30,23 @@ def create_annotation_file(dataset_directory: str, output_file: str, include_rel
 
 def copy_dataset_with_index(dataset_directory: str, output_directory: str, include_relative_path: bool = False) -> None:
     """Copy dataset with modified file names and create annotation file."""
-    annotation_file = os.path.join(output_directory, "annotation_class_index.csv")
-    
+    annotation_file = Path(output_directory) / "annotation_class_index.csv"
+
     try:
+        paths = []
+        for root, dirs, files in os.walk(dataset_directory):
+            for i, file in enumerate(files):
+                class_label = Path(root).name
+                new_name = f"{class_label}_{i:04d}.jpg"
+                source_path = Path(root) / file
+                destination_path = Path(output_directory) / new_name
+                paths.append((destination_path, destination_path.relative_to(output_directory) if include_relative_path else None, class_label))
+                shutil.copyfile(source_path, destination_path)
+
         with open(annotation_file, 'w', newline='') as csvfile:
             csvwriter = csv.writer(csvfile)
             csvwriter.writerow(['Absolute Path', 'Relative Path', 'Class Label'])
-
-            for root, dirs, files in os.walk(dataset_directory):
-                for i, file in enumerate(files):
-                    class_label = os.path.basename(root)
-                    new_name = f"{class_label}_{i:04d}.jpg"
-                    source_path = os.path.join(root, file)
-                    destination_path = os.path.join(output_directory, new_name)
-                    shutil.copyfile(source_path, destination_path)
-                    abs_path = os.path.abspath(destination_path)
-                    rel_path = os.path.relpath(abs_path, start=output_directory) if include_relative_path else None
-                    csvwriter.writerow([abs_path, rel_path, class_label])
+            csvwriter.writerows(paths)
 
         logger.info(f"Dataset with class index copied successfully. Annotation file: '{annotation_file}'.")
     except Exception as e:
@@ -52,24 +54,24 @@ def copy_dataset_with_index(dataset_directory: str, output_directory: str, inclu
 
 def copy_dataset_with_random_numbers(dataset_directory: str, output_directory: str, include_relative_path: bool = False) -> None:
     """Copy dataset with random file names and create annotation file."""
-    annotation_file = os.path.join(output_directory, "annotation_random_numbers.csv")
-    
+    annotation_file = Path(output_directory) / "annotation_random_numbers.csv"
+
     try:
+        paths = []
+        for root, dirs, files in os.walk(dataset_directory):
+            for file in files:
+                class_label = Path(root).name
+                random_number = random.randint(0, 10000)
+                new_name = f"{random_number}.jpg"
+                source_path = Path(root) / file
+                destination_path = Path(output_directory) / new_name
+                paths.append((destination_path, destination_path.relative_to(output_directory) if include_relative_path else None, class_label))
+                shutil.copyfile(source_path, destination_path)
+
         with open(annotation_file, 'w', newline='') as csvfile:
             csvwriter = csv.writer(csvfile)
             csvwriter.writerow(['Absolute Path', 'Relative Path', 'Class Label'])
-
-            for root, dirs, files in os.walk(dataset_directory):
-                for file in files:
-                    class_label = os.path.basename(root)
-                    random_number = random.randint(0, 10000)
-                    new_name = f"{random_number}.jpg"
-                    source_path = os.path.join(root, file)
-                    destination_path = os.path.join(output_directory, new_name)
-                    shutil.copyfile(source_path, destination_path)
-                    abs_path = os.path.abspath(destination_path)
-                    rel_path = os.path.relpath(abs_path, start=output_directory) if include_relative_path else None
-                    csvwriter.writerow([abs_path, rel_path, class_label])
+            csvwriter.writerows(paths)
 
         logger.info(f"Dataset with random numbers copied successfully. Annotation file: '{annotation_file}'.")
     except Exception as e:
@@ -80,4 +82,4 @@ if __name__ == "__main__":
     
     create_annotation_file(dataset_dir, "annotation_absolute_path.csv")
     copy_dataset_with_index(dataset_dir, "dataset_with_class_index", include_relative_path=True)
-    copy_dataset_with_random_numbers(dataset_dir, "dataset_with_random_numbers", include_relative_path=True)
+    copy_dataset_with_random_numbers
