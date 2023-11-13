51ba705bd22bdb589187c9bd2ac36460eb44da16
Alina <ashavarina07@gmail.com>
2023-11-13 15:48:09 +0400

    script2 final

--- Lab2/csv_name.py
+++ Lab2/csv_name.py
@@ -2,6 +2,7 @@ import csv
 import os
 import json
 import logging
+import pandas as pd
 
 logging.basicConfig(level=logging.INFO)
 
@@ -10,9 +11,10 @@ def make_csv(eggs: str) -> None:
     try:
         if not os.path.exists(eggs):
             # возвращает, True если path ссылается на существующий путь или открытый файловый дескриптор
-            with open(f'{eggs}.csv', 'a') as csvfile: 
-                csv.writer(csvfile, lineterminator="\n") # Возвращает объект writer, ответственный за преобразование 
+            with open(f'{eggs}.csv', 'w') as csvfile: 
+                spamwriter = csv.writer(csvfile, lineterminator="\n") # Возвращает объект writer, ответственный за преобразование 
                 # пользовательских данных в строки с разделителями для данного файлоподобного объекта
+                spamwriter.writerow(["Absolute path", "Relative path", "Class"])
     except:
         logging.error(f'Error in make_csv')
 
@@ -20,27 +22,27 @@ def make_list(
     folder: str, 
     search: str
 ) -> list:
-    '''принимает название папки и метки класса (cat, dog) '''
+    '''принимает название папки и метки класса (cat, dog)'''
     list = []
     for s in search:
         for photo in range(main["max_file"]):
             string = [ [ # возвращает нормализованную абсолютизированную версию имени пути path
                 os.path.abspath(os.path.join(folder, s, f"{photo:04}.jpg")), # абсолютный путь
-                os.path.join(folder, s, f"{photo:04}.jpg"), # dataset\cat\0001.jpg
+                os.path.join(folder, s, f"{photo:04}.jpg"), # dataset\cat\0000.jpg
                 s, ] # cat
             ]
             list += string
     return list
 
 def write_in_file(
-    search: list, 
+    string: list, 
     eggs: str
 ) -> None:
-    '''принимает название строк для таблицы 
+    '''принимает строки для таблицы 
     и имя папки, в которую пойдет запись'''
     try:
         make_csv(eggs)
-        for s in search:
+        for s in string:
             with open(f'{eggs}.csv', 'a', newline='') as csvfile:
                 # добавление; указатель на конце файла, если файл есть; создает новый файл, если его нет
                 spamwriter = csv.writer(csvfile, lineterminator="\n")
@@ -53,5 +55,5 @@ if __name__ == "__main__":
         # чтение; указатель на начале файла; вызывается по умолчанию
         main = json.load(main_file)
 
-    write_in_file(make_list("dataset", main["search"]), "Lab2\set\dataset_another")
+    write_in_file(make_list(main["folder"], main["search"]), main["folder_an"])
     
