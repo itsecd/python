fa323bdfd7053f43ab02852a6ae41b7aa74b5121
Quyntrd <daniil.terebilkin@gmail.com>
2023-11-14 00:47:55 +0400

    testing PathIterator

--- Lab2/5.py
+++ Lab2/5.py
@@ -1,5 +1,29 @@
-import main
+import csv
+import os
+import json
 
 
-class Iterator:
-    def __init__
+class PathIterator:
+    def __init__(self, csv_path: str, name_class: str):
+        self.data = list()
+        self.count = 0
+        self.mark = name_class
+        with open(csv_path, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if self.mark == row[2]:
+                    self.data.append(row[0])
+
+    def __iter__(self):
+        return self
+    def __next__(self) -> str:
+        if self.count < len(self.data):
+            self.count += 1
+            return self.data[self.count-1]
+        else:
+            raise StopIteration
+        
+if __name__ == "__main__":
+    with open(os.path.join("Lab2", "settings.json"), "r") as settings:
+        settings = json.load(settings)
+    iter = PathIterator(f"{settings[""]}")
