e7b5c307357b6fb34990fa3ee66d2de87e45fe3d
Prodigysy <miracle.owsyannikov@yandex.ru>
2023-11-13 22:18:44 +0400

    script2

--- Lab2/annotation's.py
+++ Lab2/annotation's.py
@@ -25,3 +25,28 @@ def create_annotation_file(dataset_directory: str, output_file: str, include_rel
         logger.info(f"Annotation file '{output_file}' created successfully.")
     except Exception as e:
         logger.exception(f"Error creating annotation file: {e}")
+
+def copy_dataset_with_index(dataset_directory: str, output_directory: str, include_relative_path: bool = False) -> None:
+    """Copy dataset with modified file names and create annotation file."""
+    annotation_file = os.path.join(output_directory, "annotation_class_index.csv")
+    
+    try:
+        with open(annotation_file, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(['Absolute Path', 'Relative Path', 'Class Label'])
+
+            for root, dirs, files in os.walk(dataset_directory):
+                for i, file in enumerate(files):
+                    class_label = os.path.basename(root)
+                    new_name = f"{class_label}_{i:04d}.jpg"
+                    source_path = os.path.join(root, file)
+                    destination_path = os.path.join(output_directory, new_name)
+                    shutil.copyfile(source_path, destination_path)
+                    abs_path = os.path.abspath(destination_path)
+                    rel_path = os.path.relpath(abs_path, start=output_directory) if include_relative_path else None
+                    csvwriter.writerow([abs_path, rel_path, class_label])
+
+        logger.info(f"Dataset with class index copied successfully. Annotation file: '{annotation_file}'.")
+    except Exception as e:
+        logger.exception(f"Error copying dataset with class index: {e}")
+
