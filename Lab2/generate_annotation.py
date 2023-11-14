028a1d9bf6afed4671f6e92bd16910dbf614f6c6
aidenpirs007@mail.ru <aidenpirs007@mail.ru>
2023-11-14 18:41:40 +0400

    Created gen. ann. file

---
+++ Lab2/generate_annotation.py
@@ -0,0 +1,18 @@
+import os
+import csv
+
+
+def generate_annotation_file(dataset_path: str, annotation_file_path: str) -> None:
+    with open(annotation_file_path, 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(['Absolute Path', 'Relative Path', 'Class'])
+
+        for root, dirs, files in os.walk(dataset_path):
+            for file in files:
+                class_name = os.path.basename(os.path.dirname(os.path.join(root, file)))
+                relative_path = os.path.relpath(os.path.join(root, file), dataset_path)
+                absolute_path = os.path.abspath(os.path.join(root, file))
+                csv_writer.writerow([absolute_path, relative_path, class_name])
+
+if __name__ == "__main__":
+    generate_annotation_file('dataset', 'annotation.csv')
