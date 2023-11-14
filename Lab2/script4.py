d9a986a1af6ab0068f98aa3c66c56f172418206b
Prodigysy <miracle.owsyannikov@yandex.ru>
2023-11-14 20:31:09 +0400

    script4

---
+++ Lab2/script4.py
@@ -0,0 +1,18 @@
+import os
+
+def get_next(name: str) -> str:
+    """
+    This function returns the relative path for the object passed to the function
+    """
+    path = os.path.join('dataset', name)
+    img_names = os.listdir(path)
+    img_names.append(None)
+    
+    for i in range(len(img_names)):
+        if img_names[i] is not None:
+            yield os.path.join(path, img_names[i])
+        elif img_names[i] is None:
+            yield None   
+
+if __name__ == "__main__":
+    print(*get_next('cat'))
