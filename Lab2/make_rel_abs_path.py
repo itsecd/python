e5a4f5bcbe98e020ce51d4e97e1eb7c6c97c6d09
fl1n4 <zhuravlev.serj2015@yandex.ru>
2023-11-14 21:06:02 +0500

    make_rel_paths

--- Lab2/make_rel_abs_path.py
+++ Lab2/make_rel_abs_path.py
@@ -14,3 +14,16 @@ def get_full_paths(class_name: str) -> List[str]:
     image_full_paths = list(
         map(lambda name: os.path.join(class_path, name).replace("\\","/"), image_names))
     return image_full_paths
+
+
+def get_rel_paths(class_name: str) -> List[str]:
+    """
+    This function returns a list of relative paths relative to the dataset file for
+    all images of a certain class passed to the function
+    """
+    rel_path = os.path.relpath('dataset')
+    class_path = os.path.join(rel_path, class_name).replace("\\","/")
+    image_names = os.listdir(class_path)
+    image_rel_paths = list(
+        map(lambda name: os.path.join(class_path, name).replace("\\","/"), image_names))
+    return image_rel_paths
