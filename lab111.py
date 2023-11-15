14e06c59975c66dfdf87f453aa82d49aecd8ece5
Arsen Suleymanian <arsens818@gmail.com>
2023-11-16 02:24:32 +0400

    Made a function get_reviews

--- lab111.py
+++ lab111.py
@@ -12,7 +12,7 @@ from fake_useragent import UserAgent
 def user_interface()->str:
     urls = []
     try:
-        print('Введите количество ссылок: ')
+        print('Введите количество фильмов , на которые хотите получить рецензии: ')
         url_quantity = int(input())
         for i in range(url_quantity):
             print('Введите', i+1, 'ссылку: ')
@@ -22,6 +22,13 @@ def user_interface()->str:
     except requests.exceptions.RequestException as e:
         logging.exception(f"Введены некоректные данные")
         return None
+def get_reviews(soup:BeautifulSoup)->List[BeautifulSoup]:
+    try:
+            reviews = soup.find('ul', class_="list-comments").find_all('li')
+            return reviews
+    except Exception as e:
+        logging.exception("Ошибка при получении списка рецензий:")
+
 
 def random_user_agent()->str:
     u = UserAgent()
@@ -34,9 +41,9 @@ def get_html_code(page:int , url:str)->BeautifulSoup:
         headers = {
             "User-Agent": random_user_agent()
         }
-        response = requests.get(tmp_url, headers=headers)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.content, "html.parser")
+        tmp_res = requests.get(tmp_url, headers=headers)
+        tmp_res.raise_for_status()
+        soup = BeautifulSoup(tmp_res.content, "html.parser")
         return soup
     except requests.exceptions.RequestException as e:
         logging.exception(f"Ошибка при получении html кода")
