240124801798c3c71871cc4c2de54b5a97ecb1ea
kksseonn <kote13241@gmail.com>
2023-11-15 15:10:52 +0400

    corrected create_annotation

--- Lab2/create_annotation.py
+++ Lab2/create_annotation.py
@@ -6,15 +6,16 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 
-def list_files_in_directory(main_folder: str) -> list:
-    """Get a list of paths to all files with the .jpg extension in the specified folder and its subfolders.
+def list_files_in_directory(main_folder: str, allowed_extensions: list = [".jpg", ".jpeg", ".png"]) -> list:
+    """Get a list of paths to all files with specified extensions in the specified folder and its subfolders.
     main_folder (str): The path to the main folder where files need to be found.
-    list(str): A list of paths to all files with the .jpg extension in the specified folder and its subfolders.
+    allowed_extensions (list): List of allowed file extensions. Default is ['.jpg', '.jpeg', '.png'].
+    list: A list of paths to all files with the specified extensions in the specified folder and its subfolders.
     """
     file_list = []
     for root, dirs, files in os.walk(main_folder):
         for file in files:
-            if file.endswith(".jpg"):
+            if any(file.lower().endswith(ext) for ext in allowed_extensions):
                 file_list.append(os.path.join(root, file))
     return file_list
 
@@ -24,7 +25,7 @@ def generate_annotation(main_folder: str) -> list:
     main_folder (str): The path to the main folder where the files are located.
     list(str): A list of annotations for each file with the absolute path, relative path, and query label.
     """
-    file_list = list_files_in_directory(main_folder)
+    file_list = list_files_in_directory(main_folder, allowed_extensions=[".jpg", ".png", ".gif"])
     annotations = []
     for file in file_list:
         absolute_path_from_project = os.path.abspath(file)
@@ -39,12 +40,10 @@ def create_annotation_file(annotation_file: str) -> None:
     """The function takes file name and create file,  if it does not exist"""
     try:
         if not os.path.exists(annotation_file):
-            with open(f"{annotation_file}.csv", "w", newline='') as file:
+            with open(f"{annotation_file}.csv", "w", newline=''):
                 pass
     except Exception as ex:
         logging.error(f"Failed to create file:: {ex}\n")
-
-# Запись аннотаций в CSV-файл
 
 
 def write_annotation_to_csv(main_folder: str, annotation_file: str) -> None:
