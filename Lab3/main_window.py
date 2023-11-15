8afcda057500fe356ba69eecdabc98fc02857057
Борис Шестериков <boris20033@gmail.com>
2023-11-15 14:39:57 +0400

    Add button for select base path

--- Lab3/main_window.py
+++ Lab3/main_window.py
@@ -1,7 +1,11 @@
 import sys
 import os
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import (QApplication, 
+                             QMainWindow, 
+                             QPushButton, 
+                             QFileDialog)
 from PyQt5.QtCore import QSize
+from make_csv import *
 
 
 class MainWindow(QMainWindow):
@@ -13,6 +17,15 @@ class MainWindow(QMainWindow):
     def GUI(self):
         self.setWindowTitle("App")
         self.setFixedSize(QSize(600, 400))
+
+
+        self.base_button = QPushButton("Select base path", self)
+        self.base_button.clicked.connect(self.select_path)
+
+
+    def select_path(self):
+        folder = QFileDialog.getExistingDirectory(self, "Select path")
+        self.base_path = os.path.basename(os.path.normpath(folder))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
