91f24775fd9e1392a49cb4e7f299c1b4588c2f42
Prodigysy <miracle.owsyannikov@yandex.ru>
2023-11-15 13:08:48 +0400

    fix iter.

--- Lab2/iter.py
+++ Lab2/iter.py
@@ -1,26 +1,39 @@
 import os
 
-class Iterator:
+class DirectoryIterator:
     def __init__(self, directory):
-        self.counter = 0
-        self.directory = directory
-        self.data = os.listdir(os.path.join('dataset', directory))
-        self.limit = len(self.data)
+        self._counter = 0
+        self._directory = directory
+        self._data = os.listdir(os.path.join('dataset', directory))
+        self._limit = len(self._data)
 
     def __iter__(self):
         return self
     
     def __next__(self):
-        if self.counter < self.limit:
-            next_path = os.path.join(self.directory, self.data[self.counter])
-            self.counter += 1
+        if self._counter < self._limit:
+            next_path = os.path.join(self._directory, self._data[self._counter])
+            self._counter += 1
             return next_path
         else:
             raise StopIteration
 
+    # Методы для доступа к полям
+    def get_counter(self):
+        return self._counter
+
+    def get_directory(self):
+        return self._directory
+
+    def get_data(self):
+        return self._data
+
+    def get_limit(self):
+        return self._limit
+
 if __name__ == "__main__":
-    cat_iterator = Iterator('cat')
-    dog_iterator = Iterator('dog')
+    cat_iterator = DirectoryIterator('cat')
+    dog_iterator = DirectoryIterator('dog')
 
     for _ in range(4):
         print(next(cat_iterator))
