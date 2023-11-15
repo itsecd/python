658c2256892e515c06ff4420bc3369ebae608baf
kksseonn <kote13241@gmail.com>
2023-11-15 16:01:51 +0400

    corrected create_copy_dataset

--- Lab2/create_copy_dataset.py
+++ Lab2/create_copy_dataset.py
@@ -3,18 +3,10 @@ import shutil
 import csv
 import json
 import logging
+import random
+from create_annotation import create_annotation_file 
 
 logging.basicConfig(level=logging.INFO)
-
-
-def create_annotation_file(new_copy_name: str) -> None:
-    """The function takes file name and create file,  if it does not exist"""
-    try:
-        if not os.path.exists(new_copy_name):
-            with open(f"{new_copy_name}.csv", "w", newline='') as file:
-                pass
-    except Exception as ex:
-        logging.error(f"Failed to create file:: {ex}\n")
 
 
 def create_folder(folder_name: str) -> None:
@@ -26,10 +18,12 @@ def create_folder(folder_name: str) -> None:
         logging.error(f"Failed to create folder:{ex.message}\n{ex.args}\n")
 
 
-def copy_dataset_with_annotation(main_folder: str, new_copy_name: str) -> None:
+def copy_dataset(main_folder: str, new_copy_name: str, copy_type: str = "numbered") -> None:
     """Copy the dataset with annotations to a new directory and create an annotation file.
-        main_folder (str): The path to the main folder where the dataset is located.
-        new_copy_name (str): The name of the new directory to which the dataset will be copied.
+    
+    main_folder (str): The path to the main folder where the dataset is located.
+    new_copy_name (str): The name of the new directory to which the dataset will be copied.
+    copy_type (str): Type of data copy. Options: "random" or "numbered".
     """
     create_folder(new_copy_name)
 
@@ -42,7 +36,15 @@ def copy_dataset_with_annotation(main_folder: str, new_copy_name: str) -> None:
                 destination_folder = os.path.join(new_copy_name, query)
                 create_folder(destination_folder)
 
-                new_filename = f"{query}_{len(os.listdir(destination_folder)) + 0:04d}.jpg"
+                if copy_type == "random":
+                    random_number = random.randint(0, 10000)
+                    new_filename = f"{random_number}.jpg"
+                elif copy_type == "numbered":
+                    new_filename = f"{query}_{len(os.listdir(destination_folder)) + 0:04d}.jpg"
+                else:
+                    logging.error("Invalid copy_type. Use 'default' or 'numbered'.")
+                    return
+
                 source_filepath = os.path.join(root, file)
                 destination_filepath = os.path.join(
                     destination_folder, new_filename)
@@ -62,5 +64,5 @@ def copy_dataset_with_annotation(main_folder: str, new_copy_name: str) -> None:
 if __name__ == "__main__":
     with open("Lab2/options.json", "r") as options_file:
         options = json.load(options_file)
-        copy_dataset_with_annotation(
-            options["main_folder"], options["new_copy_name"])
+        copy_dataset(
+            options["main_folder"], options["new_copy_name"],"numbered")
