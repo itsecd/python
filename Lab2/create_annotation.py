b059fb69891f3595c1b5a847b6d307b57d6bc42e
KsushaK <ksushak1803@gmail.com>
2023-11-15 14:24:51 +0300

    do create_annotation with setting

--- Lab2/create_annotation.py
+++ Lab2/create_annotation.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import logging
+import json
 
 logging.basicConfig(filename='annotation.log', level=logging.ERROR)
 
@@ -10,18 +11,17 @@ def create_annotation_file(dataset_path, output_file):
             csv_writer = csv.writer(csvfile)
             csv_writer.writerow(['Абсолютный путь'.ljust(100), 'Относительный путь'.ljust(100), 'Класс'.ljust(30)])
 
-            for folder_path, subfolders, files_list in os.walk(dataset_path):
+            for file_dataset, subfolders, files_list in os.walk(dataset_path):
                 for file_name in files_list:
                     if file_name.endswith('.txt'):
-                        absolute_path = os.path.abspath(os.path.join(folder_path, file_name)).ljust(100)
+                        absolute_path = os.path.abspath(os.path.join(file_dataset, file_name)).ljust(100)
                         relative_path = os.path.relpath(absolute_path, os.path.dirname(__file__)).ljust(100)
-                        class_name = os.path.basename(folder_path).ljust(30)
+                        class_name = os.path.basename(file_dataset).ljust(30)
                         csv_writer.writerow([absolute_path, relative_path, class_name])
     except Exception as e:
-        logging.error(f"Произошла ошибка: {e}", exc_info=True)
+        logging.error(f"Произошла ошибка в create_annotation_file: {e}", exc_info=True)
 
 if __name__ == "__main__":
-    dataset_folder = "C:/Users/ksush/OneDrive/Рабочий стол/python-v8/dataset"
-    annotation_file = "annotation.csv"
-
-    create_annotation_file(dataset_folder, annotation_file)
+    with open(os.path.join('Lab2', 'settings.json'), 'r') as settings:
+        settings = json.load(settings)
+    create_annotation_file(settings['normal'], settings['normal_csv'])
