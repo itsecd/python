85c44d3d25509aa51a8304fd6735100a1f86bfa4
Prodigysy <miracle.owsyannikov@yandex.ru>
2023-11-15 23:57:06 +0400

    add annotation

--- Lab2/path_of_next.py
+++ Lab2/path_of_next.py
@@ -1,22 +1,25 @@
 import os
+import csv
 from typing import Generator
 
-def get_next(name: str) -> Generator[str, None, None]:
+def get_next(class_name: str, csv_path: str) -> Generator[str, None, None]:
     """
-    This function yields the relative path for each object in the specified directory.
+    This function yields the relative path for each object in the specified directory based on the CSV file.
     """
-    path: str = os.path.join('dataset', name)
-    img_names = os.listdir(path)  # Не указываем тип явно
+    with open(csv_path, 'r') as csv_file:
+        reader = csv.reader(csv_file, delimiter=';')
+        next(reader)  
 
-    for img_name in img_names:
-        yield os.path.join(path, img_name)
-
+        for row in reader:
+            obj_class, absolute_path = row[2], row[0]
+            if class_name == obj_class:
+                yield os.path.relpath(absolute_path, 'dataset')
 
 if __name__ == "__main__":
-    cat_generator = get_next('cat')
+    csv_path = 'annotation.csv'
+    cat_generator = get_next('cat', csv_path)
 
-    next_cat_image = next(cat_generator)
-    print(next_cat_image)
-
-    next_cat_image = next(cat_generator)
-    print(next_cat_image)
+    next_cat_image = next(cat_generator, None)
+    while next_cat_image:
+        print(next_cat_image)
+        next_cat_image = next(cat_generator, None)
