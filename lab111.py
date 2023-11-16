4c3b392d4d8c57997ee7a4369f1c0916c4b018bb
Arsen Suleymanian <arsens818@gmail.com>
2023-11-16 17:05:49 +0400

    Made a function  get_html_code  and from html code get txt revie and status of it

--- lab111.py
+++ lab111.py
@@ -9,33 +9,30 @@ from fake_useragent import UserAgent
 
 
 
-def user_interface()->str:
-    urls = []
+def user_interface() -> list:
+    urls: list = []
     try:
-        print('Введите количество фильмов , на которые хотите получить рецензии: ')
+        print('Введите количество вещей, на которые хотите получить рецензии: ')
         url_quantity = int(input())
         for i in range(url_quantity):
-            print('Введите', i+1, 'ссылку: ')
+            print('Введите ссылка на ', i+1, '-юу вещь: ')
             url1 = str(input())
             urls.append(url1)
         return(urls)
     except requests.exceptions.RequestException as e:
         logging.exception(f"Введены некоректные данные")
         return None
-def get_reviews(soup:BeautifulSoup)->List[BeautifulSoup]:
-    try:
-            reviews = soup.find('ul', class_="list-comments").find_all('li')
-            return reviews
-    except Exception as e:
-        logging.exception("Ошибка при получении списка рецензий:")
 
 
-def random_user_agent()->str:
-    u = UserAgent()
-    return u.random
+def set_pages() -> int:
+    print("Со скольки страниц брать рецензии?:")
+    tmp_page=int(input())
+    return tmp_page
+
+
 def get_html_code(page:int , url:str)->BeautifulSoup:
     try:
-        tmp_url = f"{url}{page}"
+        tmp_url = url+str(page)
         sleep_time = random.uniform(1, 3)
         sleep(sleep_time)
         headers = {
@@ -48,8 +45,48 @@ def get_html_code(page:int , url:str)->BeautifulSoup:
     except requests.exceptions.RequestException as e:
         logging.exception(f"Ошибка при получении html кода")
         return None
+
+
+def get_reviews(soup: BeautifulSoup) -> List[BeautifulSoup]:
+    try:
+            reviews = soup.find('ul', class_="list-comments").find_all('li')
+            return reviews
+    except Exception as e:
+        logging.exception("Ошибка при получении списка рецензий:")
+
+
+def review_text(review: BeautifulSoup) -> str:
+    try:
+        review_txt = review.find('div', class_="reviewTextSnippet")
+        if review_txt is not None:
+            return review_txt.get_text()
+        else:
+            return "Текст рецензии не найден"
+    except Exception as e:
+        logging.exception("Ошибка при получении текста рецензии:", e)
+
+
+def status_review(review: BeautifulSoup) -> bool:
+    try:
+        stars = review.find_all(class_='on')
+        if len(stars) > 3:
+            return True
+        else:
+            return False
+    except Exception as e:
+        logging.exception("Ошибка при получении статуса рецензии:", e)
+
+
+def random_user_agent() -> str:
+    u = UserAgent()
+    return u.random
+
+
 if __name__ == "__main__":
-        urls= []
-        urls = user_interface()
-        for i in range (len(urls)):
-            print(get_html_code(1,urls[i]))
+        urls: list = user_interface()
+        pages: int = set_pages()
+        for url in urls:
+            for page in range(pages):
+                rev=get_reviews(get_html_code(page,url))
+                for review in rev:
+
