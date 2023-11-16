40c2c721123891ed607b77b05d4b6e9c9fc0b6c0
KsushaK <ksushak1803@gmail.com>
2023-11-16 05:50:55 +0300

    fixed some issues

--- Lab2/main.py
+++ Lab2/main.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
     if settings['mode'] == 'normal':
         create_annotation.create_annotation_file(settings['main_dataset'], settings['normal_csv'])
     if settings['mode'] == 'random_dataset':
-        randomized_paths = random_dataset(settings['main_dataset'],settings['dataset_random'],settings['default_number'],settings['classes'])
-        create_annotation.create_annotation_file(settings['main_dataset'], settings['random_csv'])
+        random_dataset.random_dataset(settings['main_dataset'],settings['dataset_random'],settings['default_number'],settings['classes'])
+        create_annotation.create_annotation_file(settings['dataset_random'], settings['random_csv'])
     if settings['mode'] == 'copy_dataset':
         copy_dataset(settings['main_dataset'], settings['dataset_copy'], settings['classes'])
         create_annotation.create_annotation_file((os.path.join(settings['dataset_copy'], settings['copy_csv'])))
