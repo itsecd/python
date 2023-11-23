a4c6ec89644a5097ebbc06444ff916df26ddfbe9
Вероника Будюк <veronika.budyuk2004@gmail.com>
2023-11-23 21:10:31 +0400

    done func for static and balanced

--- Lab4/main.py
+++ Lab4/main.py
@@ -33,10 +33,26 @@ def image_forms(csv_path:str, new_csv_filename:str, class_one) -> None:
         csv_file['Channels']= channels_image
         csv_file['Label']= numerical
         csv_file.to_csv(new_csv_filename, index=False)
-        print(csv_file)
+
+def checking_balance(dataframe_file:str):
+    csv_file = pd.read_csv(dataframe_file)
+    images_info=csv_file[['Height', 'Width','Channels']].describe()
+    label_stats=csv_file['Label']
+    label_info=label_stats.value_counts()
+    
+    num_images_per_label = label_info.values
+    is_balanced = (num_images_per_label[0]/num_images_per_label[1])
+    print(label_info)
+    pd.concat([images_info,label_info], axis=1).to_csv('foo.csv')
+    if ( is_balanced>=0.95 and is_balanced<=1.05):
+        print("Выборка сбалансированна с точностью:", f"{is_balanced*100:.1f}%")
+    else:
+        print("Выборка несбалансирована с точностью:", f"{is_balanced*100-100:.1f}%")
+   
 
 if __name__ == "__main__":
     with open('Lab4\settings.json', "r") as settings_file:
        settings = json.load(settings_file)
 
-    image_forms("Lab2\csv_files\dataset.csv", 'Lab4\data.csv',settings["class_one"])
+    #image_forms("Lab2\csv_files\dataset.csv", 'Lab4\data.csv',settings["class_one"])
+    checking_balance("Lab4\data.csv")
