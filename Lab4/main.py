567f353814fd84f45114f7b402a0cc34669ed3b7
Вероника Будюк <veronika.budyuk2004@gmail.com>
2023-11-23 22:46:01 +0400

    filters: min max and label, 6 and 7 points

--- Lab4/main.py
+++ Lab4/main.py
@@ -5,7 +5,7 @@ import json
 
 logging.basicConfig(level=logging.INFO)
 
-def image_forms(csv_path:str, new_csv_filename:str, class_one) -> None:
+def image_forms(csv_path:str, new_csv_filename:str, class_one) -> pd.DataFrame:
         height_image = []
         width_image = []
         channels_image = []
@@ -14,7 +14,7 @@ def image_forms(csv_path:str, new_csv_filename:str, class_one) -> None:
         csv_file = pd.read_csv(csv_path, delimiter=',', 
 	    names=['Absolute path', 'Relative path', 'Class'])  
         abs_path = csv_file['Absolute path']
-        classes=csv_file['Class']
+        
         for path_of_image in abs_path:
             img=cv2.imread(path_of_image)
             cv_tuple= img.shape
@@ -33,8 +33,9 @@ def image_forms(csv_path:str, new_csv_filename:str, class_one) -> None:
         csv_file['Channels']= channels_image
         csv_file['Label']= numerical
         csv_file.to_csv(new_csv_filename, index=False)
+        return csv_file
 
-def checking_balance(dataframe_file:str, statistic_file:str):
+def checking_balance(dataframe_file:str, statistic_file:str) -> pd.DataFrame:
     csv_file = pd.read_csv(dataframe_file)
     images_info=csv_file[['Height', 'Width','Channels']].describe()
     label_stats=csv_file['Label']
@@ -42,19 +43,29 @@ def checking_balance(dataframe_file:str, statistic_file:str):
     df = pd.DataFrame() 
     num_images_per_label = label_info.values
     is_balanced = (num_images_per_label[0]/num_images_per_label[1])
-    df['Labels']=label_info
     df['Quantity']=num_images_per_label
     df['Balance']= f'{is_balanced:.1f}'
     pd.concat([images_info,df], axis=1).to_csv(statistic_file)
     if ( is_balanced>=0.95 and is_balanced<=1.05):
-        print("Выборка сбалансированна с точностью:", f"{abs(is_balanced*100-100):.1f}%")
+        print("Выборка сбалансированна")
     else:
         print("Выборка несбалансирована с точностью:", f"{abs(is_balanced*100-100):.1f}%")
+    return df
    
+def filter_by_label(dframe, label) -> pd.DataFrame:
+    filtered_df = dframe[dframe['Label'] == label]
+    return filtered_df
+
+def min_max_filter(dframe, width_max: int, height_max: int, label: str) -> pd.DataFrame:
+    filtered_df = dframe[(dframe['Label'] == label) & (dframe['Width'] <= width_max) & (dframe['Height'] <= height_max)]
+    return filtered_df
 
 if __name__ == "__main__":
     with open('Lab4\settings.json', "r") as settings_file:
        settings = json.load(settings_file)
 
     #image_forms("Lab2\csv_files\dataset.csv", 'Lab4\data.csv',settings["class_one"])
-    checking_balance("Lab4\data.csv", "Lab4\my_stat.csv")
+    #checking_balance("Lab4\data.csv", "Lab4\my_stat.csv")
+    csv_file = pd.read_csv('Lab4\data.csv')
+    new=min_max_filter(csv_file, 200, 200, 1)
+    print(new)
