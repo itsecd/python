9ed4b07e242c357c8f4a9c325ee1817102c2459a
Влад Цуненко <vlad.tsunenko.2004@mail.ru>
2023-11-25 22:00:27 +0400

    fixed size, added message, fixed import

--- Lab3/main_window.py
+++ Lab3/main_window.py
@@ -1,14 +1,20 @@
 import os
 import sys
-import logging
 sys.path.insert(0, "Lab2")
 from datetime import datetime
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QLineEdit, QVBoxLayout, QWidget, QInputDialog, QLabel, QMessageBox
+from PyQt6.QtWidgets import (
+    QApplication, 
+    QMainWindow, 
+    QPushButton, 
+    QFileDialog, 
+    QLineEdit, 
+    QVBoxLayout, 
+    QWidget, 
+    QInputDialog, 
+    QLabel, 
+    QMessageBox)
 from main import main_function
 from get_data import get_data_for_date
-
-
-logging.basicConfig(level=logging.INFO)
 
 
 class MainWindow(QMainWindow):
@@ -16,12 +22,15 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
 
         self.setWindowTitle('Курс доллара по дате')
-        
+        self.setMaximumSize(500,200)
         self.source_folder = ""
         self.destination_folder = ""
         
-        self.select_source_folder_btn = QPushButton('Выбрать папку исходного датасета', self)
+        self.select_source_folder_btn = QPushButton('Выбрать файл исходного датасета', self)
         self.select_source_folder_btn.clicked.connect(self.select_source_folder)
+        
+        self.selected_path_label = QLabel(self)
+        self.selected_path_label.setText("Выбранный путь:")
 
         self.create_dataset_btn = QPushButton('Создать датасет', self)
         self.create_dataset_btn.clicked.connect(self.create_dataset)
@@ -34,8 +43,15 @@ class MainWindow(QMainWindow):
 
         self.data_display = QLabel(self)
         self.data_display.setText("Данные по дате:")
+        
+        self.mode_mapping = {
+            'Разбить файл на даты и данные': 'X_Y',
+            'Разбить файл по годам': 'years',
+            'Разбить файл по неделям': 'weeks'
+        }
 
         layout = QVBoxLayout()
+        layout.addWidget(self.selected_path_label)
         layout.addWidget(self.select_source_folder_btn)
         layout.addWidget(self.create_dataset_btn)
         layout.addWidget(self.date_input)
@@ -48,27 +64,36 @@ class MainWindow(QMainWindow):
 
     def select_source_folder(self) -> None:
         """Opens a dialog for selecting the source folder and writes the selected folder path to the variable"""
-        folder_path = QFileDialog.getExistingDirectory(self, 'Выберите папку исходного датасета')
+        folder_path = QFileDialog.getOpenFileName(self, 'Открыть файл', '', 'Текстовые файлы (*.csv);;Все файлы (*)')
         self.source_folder = folder_path
+        self.selected_path_label.setText(f"Выбранный путь: {self.source_folder[0]}")
 
 
     def create_dataset(self) -> None:
         """Opens a dialog for selecting a mode, destination folder, and then calls the main_function with the selected mode"""
-        modes = ['X_Y', 'years', 'weeks', 'find data']
+        modes = list(self.mode_mapping.keys())
         mode, ok = QInputDialog.getItem(self, 'Выбор режима', 'Выберите режим:', modes, 0, False)
         if ok and mode:
             self.destination_folder = QFileDialog.getExistingDirectory(self, 'Выберите папку назначения')
             while not self.destination_folder:
                 QMessageBox.warning(self, 'Внимание', 'Выберите папку назначения.')
                 self.destination_folder = QFileDialog.getExistingDirectory(self, 'Выберите папку назначения')
-            args = {
-                'mode': mode,
-                'path_file': self.destination_folder,
-                'output_x': 'X.csv',
-                'output_y': 'Y.csv',
-                'date': self.date_input.text()
-            }
-            main_function(args)
+            try:   
+                mode_internal = self.mode_mapping.get(mode)
+                if mode_internal:
+                    args = {
+                        'mode': mode_internal,
+                        'path_file': self.destination_folder,
+                        'output_x': 'X.csv',
+                        'output_y': 'Y.csv',
+                        'date': self.date_input.text()
+                    }
+                    main_function(args)
+                    QMessageBox.information(self, 'Успех', f'Операция успешно выполнена для режима: {mode}')
+                else:
+                    QMessageBox.critical(self, 'Ошибка', 'Неверный режим операции.')
+            except Exception as ex:
+                QMessageBox.critical(self, 'Ошибка', f'Произошла ошибка: {str(ex)}')
             
 
 
@@ -78,11 +103,11 @@ class MainWindow(QMainWindow):
         try:
             date = datetime.strptime(date_str, '%Y-%m-%d')
         except ValueError:
-            print(f"Invalid date format: {date_str}. Please enter the date in the format YYYY-MM-DD.")
+            print(f"Неправильный формат даты: {date_str}. Пожалуйста введите дату в формате: YYYY-MM-DD.")
             return
         args = {
             'mode': "find data",
-            'path_file': os.path.join(self.source_folder, "dataset.csv"),
+            'path_file': self.source_folder[0],
             'output_x': 'X.csv',
             'output_y': 'Y.csv',
             'date': date
