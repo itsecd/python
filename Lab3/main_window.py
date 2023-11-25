7f7183a254576275ca4fa7d0ac7e7fd06a70414a
kksseonn <kote13241@gmail.com>
2023-11-25 18:28:41 +0400

    added copy_dataset_button

--- Lab3/main_window.py
+++ Lab3/main_window.py
@@ -1,17 +1,18 @@
 import sys
 import os
 import logging
-from PyQt5 import QtWidgets, QtGui
+from PyQt6 import QtWidgets, QtGui
+
 sys.path.insert(1, 'Lab2')
 from iterator import ImageIterator
 from create_annotation import list_files_in_directory, generate_annotation, write_annotation_to_csv
-from create_copy_dataset import copy_dataset
+from create_copy_dataset import copy_dataset, CopyType
 
 logging.basicConfig(level=logging.INFO)
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super().__init__()
         self.init_ui()
 
     def init_ui(self):
@@ -19,13 +20,20 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Create widgets
         self.create_annotation_button = QtWidgets.QPushButton("Создать аннотацию")
+        self.copy_dataset_button = QtWidgets.QPushButton("Скопировать датасет")
+        self.random_copy_dataset_button = QtWidgets.QPushButton("Скопировать датасет в случайном порядке")
+        self.show_tiger_button = QtWidgets.QPushButton("Показать следующего тигра")
+        self.show_leopard_button = QtWidgets.QPushButton("Показать следующего тигра")
+        self.exit_button = QtWidgets.QPushButton("Выйти из программы")
 
         # Connect buttons to functions
         self.create_annotation_button.clicked.connect(self.create_annotation)
+        self.copy_dataset_button.clicked.connect(self.copy_dataset)
 
         # Layout setup
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(self.create_annotation_button)
+        layout.addWidget(self.copy_dataset_button)
 
         widget = QtWidgets.QWidget()
         widget.setLayout(layout)
@@ -49,9 +57,28 @@ class MainWindow(QtWidgets.QMainWindow):
                     logging.error(f"Failed to create annotation: {ex}")
                     QtWidgets.QMessageBox.critical(
                         self, 'Error', f'Failed to create annotation: {ex}')
+    
+    def copy_dataset(self):
+        main_folder = QtWidgets.QFileDialog.getExistingDirectory(
+            self, 'Выберите папку для копирования датасета:')
+
+        if main_folder:
+            new_copy_name, ok = QtWidgets.QInputDialog.getText(
+                self, 'Введите имя для новой копии датасета:', 'Новая копия')
+
+            if ok and new_copy_name:
+                try:
+                    copy_dataset(main_folder, new_copy_name, CopyType.NUMBERED)
+                    QtWidgets.QMessageBox.information(
+                        self, 'Success', 'Dataset copied successfully!')
+                except Exception as ex:
+                    logging.error(f"Failed to copy dataset: {ex}")
+                    QtWidgets.QMessageBox.critical(
+                        self, 'Error', f'Failed to copy dataset: {ex}')
+        
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
