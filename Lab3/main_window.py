1ad03f5ce2f88faa3b2fe0693035e1108bec38f8
kksseonn <kote13241@gmail.com>
2023-11-25 19:53:54 +0400

    added show_tiger_button

--- Lab3/main_window.py
+++ Lab3/main_window.py
@@ -1,14 +1,15 @@
+from create_copy_dataset import copy_dataset, CopyType
+from create_annotation import write_annotation_to_csv
+from iterator import ClassIterator
 import sys
 import os
 import logging
-from PyQt6 import QtWidgets, QtGui
+from PyQt6 import QtWidgets, QtGui, QtCore
 
 sys.path.insert(1, 'Lab2')
-from iterator import ImageIterator
-from create_annotation import list_files_in_directory, generate_annotation, write_annotation_to_csv
-from create_copy_dataset import copy_dataset, CopyType
 
 logging.basicConfig(level=logging.INFO)
+
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -19,23 +20,34 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("Dataset Annotation App")
 
         # Create widgets
-        self.create_annotation_button = QtWidgets.QPushButton("Создать аннотацию")
+        self.create_annotation_button = QtWidgets.QPushButton(
+            "Создать аннотацию")
         self.copy_dataset_button = QtWidgets.QPushButton("Скопировать датасет")
-        self.random_copy_dataset_button = QtWidgets.QPushButton("Скопировать датасет рандомно")
-        self.show_tiger_button = QtWidgets.QPushButton("Показать следующего тигра")
-        self.show_leopard_button = QtWidgets.QPushButton("Показать следующего тигра")
+        self.random_copy_dataset_button = QtWidgets.QPushButton(
+            "Скопировать датасет рандомно")
+        self.show_tiger_button = QtWidgets.QPushButton(
+            "Показать следующего тигра")
+        self.show_leopard_button = QtWidgets.QPushButton(
+            "Показать следующего леопарда")
         self.exit_button = QtWidgets.QPushButton("Выйти из программы")
+
+        # Create a QLabel to display the images
+        self.image_label = QtWidgets.QLabel(self)
+        self.image_label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
 
         # Connect buttons to functions
         self.create_annotation_button.clicked.connect(self.create_annotation)
         self.copy_dataset_button.clicked.connect(self.copy_dataset)
-        self.random_copy_dataset_button.clicked.connect(self.random_copy_dataset)
+        self.random_copy_dataset_button.clicked.connect(
+            self.random_copy_dataset)
+        self.show_tiger_button.clicked.connect(self.show_tiger)
 
         # Layout setup
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(self.create_annotation_button)
         layout.addWidget(self.copy_dataset_button)
         layout.addWidget(self.random_copy_dataset_button)
+        layout.addWidget(self.show_tiger_button)
 
         widget = QtWidgets.QWidget()
         widget.setLayout(layout)
@@ -59,7 +71,7 @@ class MainWindow(QtWidgets.QMainWindow):
                     logging.error(f"Failed to create annotation: {ex}")
                     QtWidgets.QMessageBox.critical(
                         self, 'Error', f'Failed to create annotation: {ex}')
-    
+
     def copy_dataset(self):
         main_folder = QtWidgets.QFileDialog.getExistingDirectory(
             self, 'Выберите папку для копирования датасета:')
@@ -77,6 +89,7 @@ class MainWindow(QtWidgets.QMainWindow):
                     logging.error(f"Failed to copy dataset: {ex}")
                     QtWidgets.QMessageBox.critical(
                         self, 'Error', f'Failed to copy dataset: {ex}')
+
     def random_copy_dataset(self):
         main_folder = QtWidgets.QFileDialog.getExistingDirectory(
             self, 'Выберите папку для копирования датасета:')
@@ -94,7 +107,41 @@ class MainWindow(QtWidgets.QMainWindow):
                     logging.error(f"Failed to copy dataset: {ex}")
                     QtWidgets.QMessageBox.critical(
                         self, 'Error', f'Failed to copy dataset: {ex}')
-        
+
+    def show_tiger(self):
+
+        if not hasattr(self, 'tiger_iterator') or not self.tiger_iterator:
+
+            annotation_file = 'copy_dataset.csv'
+            class_label = ["tiger"]
+
+            self.tiger_iterator = ClassIterator(annotation_file, class_label)
+
+            if not self.tiger_iterator:
+                QtWidgets.QMessageBox.critical(
+                    self, 'Error', 'Failed to initialize image iterator.')
+                return
+
+        tiger_image_path = self.tiger_iterator.next_image()
+
+        if tiger_image_path:
+            print("Showing tiger image:", tiger_image_path)
+            self.display_image(tiger_image_path)
+        else:
+            QtWidgets.QMessageBox.information(
+                self, 'Information', 'No more tiger images in the dataset.')
+
+    def display_image(self, image_path):
+        print("Displaying image:", image_path)
+        pixmap = QtGui.QPixmap(image_path)
+
+        if pixmap.isNull():
+            print("Failed to load image:", image_path)
+        else:
+            print("Image loaded successfully.")
+            self.image_label.setPixmap(pixmap)
+            self.image_label.show()
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
