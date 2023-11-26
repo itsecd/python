5013b6bb945053510e48cbd7f3e4e869c41bea31
Quyntrd <daniil.terebilkin@gmail.com>
2023-11-26 18:27:43 +0400

    imported modules

--- Lab3/1.py
+++ Lab3/1.py
@@ -1,5 +1,17 @@
-from Lab2 import create_annotation
+import sys
+import os
+import logging
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import (QApplication, QMainWindow,QPushButton,QMessageBox,QLabel,QFileDialog,QVBoxLayout,QWidget,QGridLayout)
+from PyQt6.QtGui import QPixmap
+
+sys.path.insert(1, "D:/AppProgPython/appprog/Lab2")
+from iterator import PathIterator
+from create_annotation import create_csv_list, write_into_csv
+from create_copy_folder import copy_folder
+from create_copy_random import copy_random
 
 
-create_annotation.create_csv_list()
+logging.basicConfig(level=logging.INFO)
+
 
