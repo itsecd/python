d45f5407bb0d7d225a30470ec36dbf79a81559a1
Iva Korsya <korsaeva24@gmail.com>
2023-11-27 00:59:24 +0400

    added select folders and save csv

--- Lab3/gui.py
+++ Lab3/gui.py
@@ -19,12 +19,7 @@ from annotation import write_csv
 
 
 class MainWindow(QMainWindow):
-    
-
     def __init__(self):
-
-        
-
         super(MainWindow,self).__init__()
 
         self.iter = None
@@ -34,57 +29,74 @@ class MainWindow(QMainWindow):
         self.setMaximumSize(900,800)
 
 
-        self.open_old_dir_btn = QPushButton("Open directory with dataset")
+        self.open_old_dir_btn = QPushButton('Open directory with dataset')
         self.open_old_dir_btn.clicked.connect(self.get_old_dir)
+        self.chosen_old_dir=QLabel(self)
+        self.chosen_old_dir.setFixedHeight(18)
 
-        self.open_new_dir_btn = QPushButton("Choose directory for new dataset")
+        self.open_new_dir_btn = QPushButton('Choose directory for new dataset')
         self.open_new_dir_btn.clicked.connect(self.get_new_dir)
+        self.chosen_new_dir=QLabel(self)
+        self.chosen_new_dir.setFixedHeight(18)
 
-        self.path_to_csv = QPushButton("Choose directory for csv")
+        self.path_to_csv = QPushButton('Choose directory for csv')
         self.path_to_csv.clicked.connect(self.get_csv)
+        self.chosen_csv=QLabel(self)
+        self.chosen_csv.setFixedHeight(18)
+
+        self.create_annotation_btn=QPushButton('Do the task!')
+        self.create_annotation_btn.clicked.connect(self.create_annotation)
+        self.task=QLabel(self)
+        self.task.setFixedHeight(18)
 
         self.label_ann=QComboBox(self)
         self.label_ann.addItems(['_', 'default annotation', 'copy to new dir', 'copy with random numbers'])
+        self.label_ann.currentIndexChanged.connect( self.ann_changed )
         
-        # self.index_class=0
         self.label_class=QComboBox(self)
         self.label_class.addItems(['_', '1 star', '2 stars', '3 stars', '4 stars', '5 stars'])
         self.label_class.currentIndexChanged.connect( self.index_changed )
-        # print(self.index_class)
         
         self.review_text=QLabel(self)
 
         self.next_rev = QPushButton("next review")
-        # self.next_rev.clicked.connect(self.next_review(self.iter))
         self.next_rev.clicked.connect(self.next_review)
-
-        # self.apply_btn = QPushButton("Apply")
-        # self.next_rev.clicked.connect(self.next_review)
-        # self.next_rev.clicked.connect(lambda: self.index_changed(self.label_class.currentIndex()))
 
 
         layout = QVBoxLayout()
         layout.addWidget(self.open_old_dir_btn)
+        layout.addWidget(self.chosen_old_dir)
         layout.addWidget(self.open_new_dir_btn)
+        layout.addWidget(self.chosen_new_dir)
         layout.addWidget(self.path_to_csv)
+        layout.addWidget(self.chosen_csv)
         layout.addWidget(self.label_ann)
+        layout.addWidget(self.create_annotation_btn)
+        layout.addWidget(self.task)
         layout.addWidget(self.label_class)
         layout.addWidget(self.review_text)
         layout.addWidget(self.next_rev)
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
+    
+    def ann_changed(self,i):
+        self.ann_label=i-1
+
+    def create_annotation(self):
+        self.task.setText('Task was completed, you can see reviews!')
+        write_csv(self.csv,self.ann_label,self.new_dir,self.old_dir)
 
     def index_changed(self,i):
         """
         called on dropdown change
         """
         self.index_class=i
-        self.iter = RevIterator('review.csv', i)
-        self.show_rew(self.iter.__next__())
+        self.iter = RevIterator(self.csv, i)
+        self.show_rev(self.iter.__next__())
 
 
-    def show_rew(self, path):
+    def show_rev(self, path):
         if path==None: 
             return
         with open(path, "r", encoding='utf-8') as f:
@@ -99,20 +111,23 @@ class MainWindow(QMainWindow):
 
     def next_review(self):
         path=self.iter.__next__() 
-        self.show_rew(path)
+        self.show_rev(path)
     
 
     def get_csv(self):
-         directory = QFileDialog.getSaveFileName(self,'Choose path to csv','','csv-file(*.csv)')[0]
-         print(directory)
+         self.csv = QFileDialog.getSaveFileName(self,'Choose path to csv','','csv-file(*.csv)')[0]
+         self.chosen_csv.setText(f'You chose: {self.csv}')
+         print(self.csv)
     
     def get_old_dir(self):
-        old_dir = QFileDialog.getExistingDirectory(self,'Open working directory')
-        print(old_dir)
+        self.old_dir = QFileDialog.getExistingDirectory(self,'Open working directory')
+        self.chosen_old_dir.setText(f'You chose: {self.old_dir}')
+        print(self.old_dir)
     
     def get_new_dir(self):
-        new_dir =QFileDialog.getExistingDirectory(self,'Choose directory')
-        print(new_dir)
+        self.new_dir =QFileDialog.getExistingDirectory(self,'Choose directory')
+        self.chosen_new_dir.setText(f'You chose: {self.new_dir}')
+        print(self.new_dir)
         
     def closeEvent(self, event):
         event.accept()
