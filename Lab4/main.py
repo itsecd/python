f4c4d9c5505af3b1b303591a2d87e5f6e2dc97ee
Prodigysy <miracle.owsyannikov@yandex.ru>
2023-11-27 01:16:09 +0400

    add ds and main

--- Lab4/main.py
+++ Lab4/main.py
@@ -7,6 +7,15 @@ import random
 import matplotlib.pyplot as plt
 
 def create_dataframe(data_folder):
+    """
+    Создает DataFrame, содержащий две колонки: 'Class' и 'Path'.
+
+    Parameters:
+    - data_folder (str): Путь к папке с данными.
+
+    Returns:
+    - pd.DataFrame: DataFrame с информацией о классах и путях к изображениям.
+    """
     data = {'Class': [], 'Path': []}
 
     for class_label in os.listdir(data_folder):
@@ -20,20 +29,56 @@ def create_dataframe(data_folder):
     return pd.DataFrame(data)
 
 def add_numeric_label_column(df):
+    """
+    Добавляет числовой столбец 'NumericLabel' в DataFrame.
+
+    Parameters:
+    - df (pd.DataFrame): Исходный DataFrame.
+
+    Returns:
+    - pd.DataFrame: DataFrame с добавленным столбцом 'NumericLabel'.
+    """
     df['NumericLabel'] = df['Class'].apply(lambda x: 0 if x == 'cat' else 1)
     return df
 
 def add_image_dimensions_columns(df):
+    """
+    Добавляет столбцы 'Height', 'Width' и 'Depth' в DataFrame, содержащие информацию о размерах изображений.
+
+    Parameters:
+    - df (pd.DataFrame): Исходный DataFrame.
+
+    Returns:
+    - pd.DataFrame: DataFrame с добавленными столбцами.
+    """
     df['Height'] = df['Path'].apply(lambda x: get_image_dimensions(x)[0])
     df['Width'] = df['Path'].apply(lambda x: get_image_dimensions(x)[1])
     df['Depth'] = df['Path'].apply(lambda x: get_image_dimensions(x)[2])
     return df
 
 def get_image_dimensions(image_path):
+    """
+    Получает размеры изображения (высоту, ширину и глубину) с использованием библиотеки Pillow.
+
+    Parameters:
+    - image_path (str): Путь к изображению.
+
+    Returns:
+    - tuple: Кортеж с высотой, шириной и глубиной изображения.
+    """
     with Image.open(image_path) as img:
         return img.height, img.width, len(img.getbands())
 
 def calculate_statistics(df):
+    """
+    Вычисляет статистическую информацию о размерах изображений и метках класса.
+
+    Parameters:
+    - df (pd.DataFrame): Исходный DataFrame.
+
+    Returns:
+    - pd.DataFrame: DataFrame со статистикой.
+    """
     statistics = df.groupby('Class').agg({
         'Height': ['mean', 'min', 'max'],
         'Width': ['mean', 'min', 'max'],
@@ -43,24 +88,79 @@ def calculate_statistics(df):
     return statistics
 
 def filter_dataframe_by_class(df, class_label):
+    """
+    Фильтрует DataFrame по метке класса.
+
+    Parameters:
+    - df (pd.DataFrame): Исходный DataFrame.
+    - class_label (str): Метка класса для фильтрации.
+
+    Returns:
+    - pd.DataFrame: Отфильтрованный DataFrame.
+    """
     filtered_df = df[df['Class'] == class_label]
     return filtered_df
 
 def filter_dataframe_by_dimensions(df, class_label, max_width, max_height):
+    """
+    Фильтрует DataFrame по размерам изображений и метке класса.
+
+    Parameters:
+    - df (pd.DataFrame): Исходный DataFrame.
+    - class_label (str): Метка класса для фильтрации.
+    - max_width (int): Максимальная ширина изображения.
+    - max_height (int): Максимальная высота изображения.
+
+    Returns:
+    - pd.DataFrame: Отфильтрованный DataFrame.
+    """
     filtered_df = df[(df['Class'] == class_label) & (df['Width'] <= max_width) & (df['Height'] <= max_height)]
     return filtered_df
 
 def calculate_pixel_statistics(df):
+    """
+    Группирует DataFrame по метке класса с вычислением статистики по количеству пикселей.
+
+    Parameters:
+    - df (pd.DataFrame): Исходный DataFrame.
+
+    Returns:
+    - pd.DataFrame: DataFrame со статистикой по количеству пикселей.
+    """
     df['PixelCount'] = df.apply(lambda row: calculate_image_pixel_count(row['Height'], row['Width'], row['Depth']), axis=1)
     pixel_statistics = df.groupby('Class')['PixelCount'].agg(['mean', 'min', 'max'])
     return pixel_statistics
 
+def calculate_image_pixel_count(height, width, depth):
+    """
+    Вычисляет количество пикселей на изображении.
+
+    Parameters:
+    - height (int): Высота изображения.
+    - width (int): Ширина изображения.
+    - depth (int): Глубина изображения (количество каналов).
+
+    Returns:
+    - int: Количество пикселей.
+    """
+    return height * width * depth
+
 def generate_histogram(df, class_label):
+    """
+    Генерирует гистограмму для случайного изображения из DataFrame.
+
+    Parameters:
+    - df (pd.DataFrame): Исходный DataFrame.
+    - class_label (str): Метка класса изображения.
+
+    Returns:
+    - list: Список массивов значений гистограммы для каждого канала.
+    """
     # Выбор случайного изображения из DataFrame для указанного класса
     random_image = df[df['Class'] == class_label].sample(1)
 
     # Получение пути к изображению
-    image_path = random_image.iloc[0]['ImagePath']
+    image_path = random_image.iloc[0]['Path']
 
     # Чтение изображения с использованием OpenCV
     image = cv2.imread(image_path)
@@ -74,7 +174,12 @@ def generate_histogram(df, class_label):
     return histogram_data
 
 def plot_histogram(histogram_data):
-    # Отрисовка гистограммы с использованием matplotlib
+    """
+    Отрисовывает гистограмму с использованием matplotlib.
+
+    Parameters:
+    - histogram_data (list): Список массивов значений гистограммы для каждого канала.
+    """
     plt.figure(figsize=(10, 5))
     colors = ['red', 'green', 'blue']
 
@@ -87,16 +192,30 @@ def plot_histogram(histogram_data):
     plt.legend()
     plt.show()
 
-def calculate_image_pixel_count(height, width, depth):
-    return height * width * depth
+if __name__ == "__main__":
+    # Пример использования функций
+    data_folder = "путь_к_вашим_данным"
+    df = create_dataframe(data_folder)
+    df = add_numeric_label_column(df)
+    df = add_image_dimensions_columns(df)
 
-# Пример использования функций
-df = create_dataframe()
-df = add_numeric_label_column(df)
-df = add_image_dimensions_columns(df)
-statistics = calculate_statistics(df)
-filtered_df = filter_dataframe_by_class(df, 'cat')
-filtered_df_by_dimensions = filter_dataframe_by_dimensions(df, 'cat', 500, 500)
-pixel_statistics = calculate_pixel_statistics(df)
-histogram_data = generate_histogram(df, 'cat')
-plot_histogram(histogram_data)
+    # Вывод статистики
+    print("Statistics:")
+    print(calculate_statistics(df))
+
+    # Фильтрация DataFrame
+    filtered_df_class = filter_dataframe_by_class(df, 'cat')
+    filtered_df_dimensions = filter_dataframe_by_dimensions(df, 'cat', 500, 500)
+    print("\nFiltered DataFrame by Class:")
+    print(filtered_df_class.head())
+    print("\nFiltered DataFrame by Dimensions:")
+    print(filtered_df_dimensions.head())
+
+    # Статистика по количеству пикселей
+    pixel_statistics = calculate_pixel_statistics(df)
+    print("\nPixel Statistics:")
+    print(pixel_statistics)
+
+    # Генерация и отрисовка гистограммы
+    histogram_data = generate_histogram(df, 'cat')
+    plot_histogram(histogram_data)
