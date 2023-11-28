7ecf67aef45e99b63ddf04101ab512abe1c836d9
KsushaK <ksushak1803@gmail.com>
2023-11-28 20:23:21 +0300

    .

--- Lab3/main_window.py
+++ Lab3/main_window.py
@@ -3,7 +3,7 @@ import os
 import logging
 from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QFileDialog, QMessageBox, QTextBrowser, QComboBox, QInputDialog
 
-# Importing functions from other modules
+
 sys.path.insert(1, "C:\\Users\\ksush\\OneDrive\\Рабочий стол\\python-v8\\Lab2")
 from create_annotation import create_annotation_file
 from random_dataset import random_dataset
@@ -13,46 +13,39 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.iter = None
-        # Initializing variables
         self.dataset_path = ""
         self.annotation_file_path = ""
         self.randomized_dataset_path = ""
         self.dataset_iterator = None
         self.classes = ["bad", "good"]
         self.default_size = 1000
-        # Создание ComboBox и добавление вариантов
         self.combo = QComboBox(self)
         self.combo.addItems(self.classes)
         self.combo.setCurrentIndex(0)
 
-        # Initializing the user interface
         self.init_ui()
 
     def init_ui(self):
         self.setWindowTitle('Dataset Application')
         self.setGeometry(100, 100, 400, 300)
 
-        # Buttons
         self.browse_dataset_btn = QPushButton('Select Data Folder', self)
         self.create_annotation_btn = QPushButton('Create Annotation', self)
         self.create_random_dataset_btn = QPushButton('Create Random Dataset', self)
         self.next_good_review_btn = QPushButton('Next Positive Review', self)
         self.next_bad_review_btn = QPushButton('Next Negative Review', self)
 
-        # Connecting button signals to methods
         self.browse_dataset_btn.clicked.connect(self.browse_dataset)
         self.create_annotation_btn.clicked.connect(self.create_annotation)
         self.create_random_dataset_btn.clicked.connect(self.create_random_dataset)
         self.next_good_review_btn.clicked.connect(lambda: self.next('good'))
         self.next_bad_review_btn.clicked.connect(lambda: self.next('bad'))
 
-        # Display image
         self.txt_file = QLabel(self)
         self.text_label = QTextBrowser(self)
         self.text_label.setText("Здесь будет отзыв")
         self.text_label.setFixedSize(600, 400)
-        
-        # Layout для кнопок и других элементов
+
         layout = QVBoxLayout()
         layout.addWidget(self.browse_dataset_btn)
         layout.addWidget(self.create_annotation_btn)
@@ -60,9 +53,8 @@ class MainWindow(QMainWindow):
         layout.addWidget(self.next_bad_review_btn)
         layout.addWidget(self.next_good_review_btn)
         layout.addWidget(self.txt_file)
-        layout.addWidget(self.text_label)  # Добавление text_label в этот layout
+        layout.addWidget(self.text_label)  
 
-        # Установка layout в качестве центрального виджета
         central_widget = QWidget(self)
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
@@ -112,7 +104,7 @@ class MainWindow(QMainWindow):
         self.dataset_path = QFileDialog.getExistingDirectory(self, "Select Data Folder")
         if self.dataset_path:
             dataset_iterator = self.get_dataset_files()
-            dataset_files = list(dataset_iterator)  # Convert generator to list
+            dataset_files = list(dataset_iterator)  
             self.iter = FileIterator(dataset_files)
 
     def get_dataset_files(self):
@@ -144,7 +136,7 @@ class MainWindow(QMainWindow):
             return
 
         self.text_label.update()
-        # Logic to display the file content in the widget
+        
         with open(self.review_path, 'r', encoding='utf-8') as file:
             self.txt_file.setText(self.review_path)
             self.text_label.setText(file.read())
