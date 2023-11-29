e1081c2118aa5e10d5661d583b1ca04694b10103
Борис Шестериков <boris20033@gmail.com>
2023-11-29 15:33:43 +0400

    Add Height, Width and Channels of Img to DataFrame

--- Lab4/main.py
+++ Lab4/main.py
@@ -1,12 +1,20 @@
 import pandas as pd
+import cv2
 from csv_open_save import open_csv, save_csv 
 
 def make_dframe(csv_path : str):
     dframe = open_csv(csv_path)
     abs_path = dframe["Absolute path"]
+    height = []
+    width = []
+    channels = []
     type = []
     counter = 0
-    for img in abs_path:
+    for path in abs_path:
+        img = cv2.imread(path)
+        height.append(img.shape[0])
+        height.append(img.shape[1])
+        height.append(img.shape[2])
         if(dframe[counter, "Class"] == "rose"):
             type.append(0)
         elif(dframe[counter, "Class"] == "tulip"):
@@ -14,6 +22,9 @@ def make_dframe(csv_path : str):
 
         counter+=1
 
+    dframe["Height"] = height
+    dframe["Width"] = width
+    dframe["Channels"] = channels
     dframe["Type"] = type
 
     return dframe
