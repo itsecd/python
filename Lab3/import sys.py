cc7ba386c5a33a67591114625c5a73807f920da5
John Doe <johndoe@email.com>
2023-12-07 16:46:04 +0300

    DateApp

--- Lab3/import sys.py
+++ Lab3/import sys.py
@@ -9,6 +9,43 @@ from get_value_form_date import read_data_for_date
 from spliting_into_two_files import split_csv_by_columns
 
 
+class DateApp(QWidget):
+    def __init__(self):
+        """
+        Initialize the DateApp widget.
+        """
+        super().__init__()
+
+        self.data_path = None
+
+        self.date_label = QLabel('Select date:')
+        self.date_edit = QDateEdit()
+        self.date_edit.setDate(QDate.currentDate())  # Set the current date by default
+
+        self.browse_data_button = QPushButton('Browse Data', self)
+        self.browse_data_button.clicked.connect(self.browse_data)
+
+        self.select_datafile = QLabel('No data file selected')
+
+        self.result_label = QLabel('Data for the selected date will be displayed here')
+
+        self.get_data_button = QPushButton('Get Data')
+        self.get_data_button.clicked.connect(self.show_data)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.date_label)
+        layout.addWidget(self.date_edit)
+        layout.addWidget(self.browse_data_button)
+        layout.addWidget(self.select_datafile)
+        layout.addWidget(self.get_data_button)
+        layout.addWidget(self.result_label)
+
+        self.setLayout(layout)
+
+        self.setWindowTitle('GetDataByDate')
+        self.setGeometry(300, 300, 400, 200)
+
+
 def show_data(self) -> None:
         """
         Display data for the selected date.
