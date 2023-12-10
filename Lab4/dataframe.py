565f5e9bf5a7f6e22733b891949fc383e9a71321
Yana Yakovleva <Yakovlevaya11.06@gmail.com>
2023-12-11 01:08:36 +0400

    No, I still figured it out - done dataframe

--- Lab4/dataframe.py
+++ Lab4/dataframe.py
@@ -9,40 +9,39 @@ logging.basicConfig(filename="log.log", filemode="a", level=logging.INFO)
 
 def create_dataframe(csv_path: str, csv_filename: str, tag: str) -> None:
     print('in function')
-    csv_file=pd.read_csv(csv_path,delimiter=',',names=['Absolute path','Relative path','Tag'])
-    abs_path=csv_file['Absolute path']
-    tag=csv_file['Tag']
-    height_img=[]
-    width_img=[]
-    depth_img=[]
-    labels=[]
-    count=0
+    csv_file = pd.read_csv(csv_path, delimiter=',', names=['Absolute path', 'Relative path', 'Tag'])
+    abs_path = csv_file['Absolute path']
+    tags = csv_file['Tag']
+    height_img = []
+    width_img = []
+    depth_img = []
+    labels = []
+    count = 0
     try:
         for img in abs_path:
-            path=cv2.imread(img)
-            tuple=path.shape
-            print(tuple)
+            path = cv2.imread(img)
+            tuple = path.shape
             height_img.append(tuple[0])
             width_img.append(tuple[1])
             depth_img.append(tuple[2])
-            if csv_file.loc[count,'Tag']==tag:
-                label=0
-            else: 
-                label=1
+            if tags[count] == tag:
+                label = 0
+            else:
+                label = 1
             labels.append(label)
-            count+=1
+            count += 1
     except Exception as e:
         logging.error(f'Error get img by idx{e}')
-    csv_file=csv_file.drop('Relative path',axis=1)
+    csv_file = csv_file.drop('Relative path', axis=1)
     csv_file['Height'] = height_img
     csv_file['Width'] = width_img
     csv_file['Channels'] = depth_img
     csv_file['Label'] = labels
-    csv_file.to_csv(csv_filename,idx=False)
+    csv_file.to_csv(csv_filename, index=False)
     print(csv_file)
 
 if __name__=="__main__":
     with open('settings.json','r') as f:
         settings=json.load(f)
-    create_dataframe(os.path.join(r'C:\Users\Yana\Documents\python-v6\Lab2', 'file.csv'), 'Lab4\data.csv', settings['tag_tiger'])
+    create_dataframe(os.path.join(r'C:\Users\Yana\Documents\python-v6\Lab2', 'file.csv'), 'data.csv', settings['tag_tiger'])
    
