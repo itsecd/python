865c4a2adc58b737f627235f77b85a2488548a94
Darktezi <Darktezi@mail.ru>
2023-12-11 15:40:10 +0400

    add statistics and group functions

--- Lab4/csv_file_processing.py
+++ Lab4/csv_file_processing.py
@@ -1,45 +1,28 @@
 import pandas as pd
 
 
-def read_data(file_path):
+def preparing_dataframe(file_path):
     data = pd.read_csv(file_path)
-    return data
-
-
-def rename_columns(data):
     data.columns = ['Date', 'Value']
-    return data
 
-
-def remove_invalid_values(data):
     invalid_values = ['NaN', None, 'Page not found']  # Список невалидных значений для проверки
-
     for column in data.columns:
         data = data[~data[column].isin(invalid_values)]
-
     data.reset_index(drop=True, inplace=True)
-    return data
 
-
-def add_deviation_columns(data):
     data['Value'] = pd.to_numeric(data['Value'], errors='coerce')
     data = data.dropna(subset=['Value'])
-
     median_value = data['Value'].median()
     mean_value = data['Value'].mean()
-
     data['Median_Deviation'] = data['Value'] - median_value
     data['Mean_Deviation'] = data['Value'] - mean_value
+    statistics = data[['Value', 'Median_Deviation', 'Mean_Deviation']].describe()
 
-    return data
+    return statistics
 
 
 def main(file_path):
-    data = read_data(file_path)
-    data = rename_columns(data)
-    data = remove_invalid_values(data)
-    data = add_deviation_columns(data)
-
+    data = preparing_dataframe(file_path)
     return data
 
 
