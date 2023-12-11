01a4cb7d6880cf42653fc2c38005339d2d118e7f
Darktezi <Darktezi@mail.ru>
2023-12-11 16:20:13 +0400

    create monthly graph function

--- Lab4/creating_graphs.py
+++ Lab4/creating_graphs.py
@@ -16,3 +16,21 @@ def create_graph(file_path):
 
     plt.tight_layout()
     plt.show()
+
+
+def create_monthly_graph(dataframe, month):
+    dataframe['Date'] = pd.to_datetime(dataframe['Date'])
+    filtered_data = dataframe[dataframe['Date'].dt.month == month]
+
+    plt.figure(figsize=(10, 6)) 
+    plt.plot(filtered_data['Date'], filtered_data['Value'], label='Значение курса', color='blue') 
+    plt.axhline(y=filtered_data['Value'].median(), color='red', linestyle='--', label='Медиана') 
+    plt.axhline(y=filtered_data['Value'].mean(), color='green', linestyle='--', label='Среднее значение')
+
+    plt.xlabel('Дата')
+    plt.ylabel('Значение курса')
+    plt.title(f'Изменение курса в месяце {month}')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
