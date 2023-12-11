7f72172ecb5b0d879716e098e6d3e4db6b4e952b
Darktezi <Darktezi@mail.ru>
2023-12-11 15:31:03 +0400

    add columns Median and Mean deviation

--- Lab4/csv_file_processing.py
+++ Lab4/csv_file_processing.py
@@ -21,9 +21,28 @@ def remove_invalid_values(data):
     return data
 
 
+def add_deviation_columns(data):
+    data['Value'] = pd.to_numeric(data['Value'], errors='coerce')
+    data = data.dropna(subset=['Value'])
+
+    median_value = data['Value'].median()
+    mean_value = data['Value'].mean()
+
+    data['Median_Deviation'] = data['Value'] - median_value
+    data['Mean_Deviation'] = data['Value'] - mean_value
+
+    return data
+
+
 def main(file_path):
     data = read_data(file_path)
     data = rename_columns(data)
     data = remove_invalid_values(data)
+    data = add_deviation_columns(data)
 
     return data
+
+
+file_path = "Lab4/dataset/data.csv"
+resulting_dataframe = main(file_path)
+print(resulting_dataframe)
