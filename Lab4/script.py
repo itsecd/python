bae82b07b13cfadad86ba97831c049f068046c0c
Iva Korsya <korsaeva24@gmail.com>
2023-12-12 18:19:42 +0400

    added histogramm

--- Lab4/script.py
+++ Lab4/script.py
@@ -1,10 +1,12 @@
-
+import argparse
 import pandas as pd
 import csv
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import logging
+import os
+
 
 def make_data_frame(path_csv='1.csv') -> pd.DataFrame:
     """
@@ -13,14 +15,16 @@ def make_data_frame(path_csv='1.csv') -> pd.DataFrame:
     texts = []
     stars = []
     cnt_words = []
-    with open(path_csv, 'r', newline='') as csvfile:
-        for rev in csv.reader(csvfile, delimiter=','):
-            stars.append(rev[2])
-            with open(rev[0], 'r', encoding='utf-8') as f:
-                text = f.readline()
-                texts.append(text)
-                cnt_words.append(len(text.split()))
-
+    try:
+        with open(path_csv, 'r', newline='') as csvfile:
+            for rev in csv.reader(csvfile, delimiter=','):
+                stars.append(rev[2])
+                with open(rev[0], 'r', encoding='utf-8') as f:
+                    text = f.readline()
+                    texts.append(text)
+                    cnt_words.append(len(text.split()))
+    except:
+        logging.error('error in make_data_frame')
     df = pd.DataFrame({'review': texts, 'count words': cnt_words, 'stars': stars}).dropna()
     return df
 
@@ -46,40 +50,42 @@ def group(data: pd.DataFrame) -> pd.DataFrame:
     return data.groupby('stars').agg({'count words': ['max', 'min', 'mean']})
 
 
-def make_hist(df: pd.DataFrame, star: int) ->list:
+def make_hist(df: pd.DataFrame, star: int) -> list:
+    """
+    does lemmatic analysis
+    """
     lemmatizer = WordNetLemmatizer()
-    lemmas=[]
-    data=filter_by_class(star)
-    for i in range(0, len(data)):
-        words = word_tokenize(data['review'].values[i])
-        for word in words:
-            if (ord(word[0]) > 191):
-                lemma = lemmatizer.lemmatize(word)
-                lemmas.append(lemma)
+    lemmas = []
+    data = filter_by_class(df, star)
+    for i in range(len(data)):
+        for word in word_tokenize(data['review'].values[i]):
+            if ord(word[0]) > 191:
+                lemmas.append(lemmatizer.lemmatize(word))
     return lemmas
 
 
 def get_statistic(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    get statistic about numeric cols
+    """
     try:
         return data[['count words']].describe()
     except:
         logging.error('error in get_statistic')
+
+
 def draw_hists(df: pd.DataFrame) -> None:
     """
     draw histogramm
     """
-    plt.plot(len(make_hist(df,1)), color='blue', label='Blue')
-    plt.plot(len(make_hist(df,2)), color='green', label='Green')
-    plt.plot(len(make_hist(df,3)), color='blue', label='Red')
-    plt.plot(len(make_hist(df,4)), color='blue', label='Red')
-    plt.plot(len(make_hist(df,5)), color='blue', label='Red')
-
-    plt.xlabel("intensity")
-    plt.ylabel("density")
-    plt.title("Histograms")
+    plt.bar(range(1, 6), [len(make_hist(df, i)) for i in range(1, 6)])
     plt.legend()
     plt.show()
 
+
 if __name__ == '__main__':
-    print(get_statistic(make_data_frame()))
-    #draw_hists(make_data_frame())
+    logging.basicConfig(level=logging.INFO, filename=os.path.join('py_log.log'), filemode='w')
+    parser = argparse.ArgumentParser(description="Input csv path, label of class")
+    parser.add_argument("-c", "--csv", help="Input csv path", type=str)
+    args = parser.parse_args()
+    draw_hists(make_data_frame(args.csv))
