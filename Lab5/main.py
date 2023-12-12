0d28d6ebe71f0cb3d4cf42c9dd89e0e58fe1f2b4
Борис Шестериков <boris20033@gmail.com>
2023-12-12 13:28:03 +0400

    import and fix gitignore

---
+++ Lab5/main.py
@@ -0,0 +1,11 @@
+import os
+import numpy as np
+import pandas as pd
+import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torchvision import transforms
+from matplotlib import pyplot as plt
+from PIL import Image
