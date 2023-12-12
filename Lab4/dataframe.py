ede6412fd8d409006d79d4e6c18d8e0960c8c9c4
kksseonn <kote13241@gmail.com>
2023-12-12 13:30:12 +0400

    added task 5

--- Lab4/dataframe.py
+++ Lab4/dataframe.py
@@ -21,8 +21,21 @@ def create_dataframe(csv_path):
     result_df.columns = ['class_name', 'absolute_path', 'numeric_label', 'height', 'width', 'depth']
     return result_df
 
+def compute_statistics(df):
+    image_dimensions_stats = df[['height', 'width', 'depth']].describe()
+    print("Statistics for image sizes:")
+    print(image_dimensions_stats)
+
+    class_label_stats = df['numeric_label'].value_counts()
+    print("\nStatistics for class labels:")
+    print(class_label_stats)
+
+    is_balanced = class_label_stats.min() == class_label_stats.max()
+    print("\nThe data set is balanced:", is_balanced)
+
 if __name__ == "__main__":
     with open("Lab4/options.json", "r") as options_file:
         options = json.load(options_file)
     df = create_dataframe(options['annotation'])
     print(df)
+    compute_statistics(df)
