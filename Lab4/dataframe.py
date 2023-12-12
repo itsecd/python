a25b4e66a1daf3609c2c680f4d83c9780d01a8fd
kksseonn <kote13241@gmail.com>
2023-12-12 19:44:39 +0400

    added logging, dosctring, type-hinting.

--- Lab4/dataframe.py
+++ Lab4/dataframe.py
@@ -4,127 +4,235 @@ from PIL import Image
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import logging
 
 
-def get_image_dimensions(image_path):
-    with Image.open(image_path) as img:
-        width, height = img.size
-        channels = len(img.getbands())
-    return height, width, channels
+logging.basicConfig(level=logging.INFO)
 
 
-def get_pixel_count(image_path):
-    img = cv2.imread(image_path)
-    if img is not None:
-        return img.size // img.itemsize
-    else:
-        print(f"Warning: Unable to read image at path {image_path}")
+def get_image_dimensions(image_path: str) -> tuple[int, int, int]:
+    """
+    Get the dimensions of an image. 
+    Accepts entry: The path to the image. 
+    Returns: Image height, width, and channels
+    """
+    try:
+        with Image.open(image_path) as img:
+            width, height = img.size
+            channels = len(img.getbands())
+        logging.info(
+            f"Image dimensions for {image_path}: {width}x{height}, Channels: {channels}")
+        return height, width, channels
+    except Exception as e:
+        logging.warning(f"Error in get_image_dimensions for {image_path}: {e}")
+        return 0, 0, 0
+
+
+def get_pixel_count(image_path: str) -> int:
+    """
+    Get the pixel count of an image. 
+    Accepts entry: image_path (str): The path to the image. 
+    Returns: int: Pixel count
+    """
+    try:
+        img = cv2.imread(image_path)
+        if img is not None:
+            pixel_count = img.size // img.itemsize
+            logging.info(f"Pixel count for {image_path}: {pixel_count}")
+            return pixel_count
+        else:
+            logging.warning(f"Unable to read image at path {image_path}")
+            return 0
+    except Exception as e:
+        logging.warning(f"Error in get_pixel_count for {image_path}: {e}")
         return 0
 
 
-def create_dataframe(csv_path):
-    df = pd.read_csv(csv_path, header=None, names=[
-                     'absolute_path', 'relative_path', 'class'], sep=',')
-    df['label'] = df['class'].factorize()[0]
+def create_dataframe(csv_path: str) -> pd.DataFrame:
+    """
+    Create a DataFrame from a CSV file containing image annotations. 
+    Accepts entry: csv_path (str): Path to the CSV file. 
+    Returns: pd.DataFrame: DataFrame containing image information.
+    """
+    try:
+        df = pd.read_csv(csv_path, header=None, names=[
+                         'absolute_path', 'relative_path', 'class'], sep=',')
+        df['label'] = df['class'].factorize()[0]
 
-    class_mapping = {'leopard': 0, 'tiger': 1}
-    df['numeric_label'] = df['class'].map(class_mapping)
+        class_mapping = {'leopard': 0, 'tiger': 1}
+        df['numeric_label'] = df['class'].map(class_mapping)
 
-    df['height'], df['width'], df['depth'] = zip(
-        *df['absolute_path'].apply(get_image_dimensions))
-    df['pixel_count'] = df['absolute_path'].apply(get_pixel_count)
+        df['height'], df['width'], df['depth'] = zip(
+            *df['absolute_path'].apply(get_image_dimensions))
 
-    result_df = df[['class', 'absolute_path', 'numeric_label',
-                    'height', 'width', 'depth', 'pixel_count']].copy()
-    result_df.columns = ['class_name', 'absolute_path',
-                         'numeric_label', 'height', 'width', 'depth', 'pixel_count']
-    return result_df
+        df['pixel_count'] = df['absolute_path'].apply(get_pixel_count)
 
+        result_df = df[['class', 'absolute_path', 'numeric_label',
+                        'height', 'width', 'depth', 'pixel_count']].copy()
+        result_df.columns = ['class_name', 'absolute_path',
+                             'numeric_label', 'height', 'width', 'depth', 'pixel_count']
 
-def compute_statistics(df):
-    image_dimensions_stats = df[['height', 'width',
-                                 'depth', 'pixel_count']].describe()
-    print("Statistics for image sizes and pixel count:")
-    print(image_dimensions_stats)
-
-    class_label_stats = df['numeric_label'].value_counts()
-    print("\nStatistics for class labels:")
-    print(class_label_stats)
-
-    is_balanced = class_label_stats.min() == class_label_stats.max()
-    print("\nThe data set is balanced:", is_balanced)
+        logging.info("DataFrame created successfully.")
+        return result_df
+    except Exception as e:
+        logging.error(f"Error in create_dataframe: {e}")
+        return pd.DataFrame()
 
 
-def filter_dataframe(input_df, target_label=None, max_height=None, max_width=None):
-    filtered_df = input_df.copy()
+def compute_statistics(df: pd.DataFrame) -> None:
+    """Compute statistics for the DataFrame. Parameters: df (pd.DataFrame): Input DataFrame"""
+    try:
+        image_dimensions_stats = df[['height', 'width',
+                                     'depth', 'pixel_count']].describe()
+        logging.info("Statistics for image sizes and pixel count:")
+        logging.info(image_dimensions_stats)
 
-    if target_label is not None:
-        filtered_df = filtered_df[filtered_df['numeric_label'] == target_label]
+        class_label_stats = df['numeric_label'].value_counts()
+        logging.info("Statistics for class labels:")
+        logging.info(class_label_stats)
 
-    if max_height is not None:
-        filtered_df = filtered_df[filtered_df['height'] <= max_height]
-
-    if max_width is not None:
-        filtered_df = filtered_df[filtered_df['width'] <= max_width]
-
-    return filtered_df
-
-
-def group_by_label_and_pixel_count(df):
-    grouped_df = df.groupby('numeric_label')['pixel_count'].agg(
-        ['min', 'max', 'mean']).reset_index()
-    grouped_df.columns = ['numeric_label', 'min_pixel_count',
-                          'max_pixel_count', 'mean_pixel_count']
-    return grouped_df
+        is_balanced = class_label_stats.min() == class_label_stats.max()
+        logging.info("The data set is balanced: %s", is_balanced)
+    except Exception as e:
+        logging.error(f"Error in compute_statistics: {e}")
 
 
-def plot_histogram(dataframe, target_label):
-    filtered_df = dataframe[dataframe['numeric_label'] == target_label]
-    random_image_path = np.random.choice(filtered_df['absolute_path'].values)
-    image = cv2.imread(random_image_path)
+def filter_dataframe(input_df: pd.DataFrame, target_label: int = None, max_height: int = None, max_width: int = None) -> pd.DataFrame:
+    """
+    Filter the DataFrame based on specified criteria.
 
-    b, g, r = cv2.split(image)
+    Parameters:
+        input_df (pd.DataFrame): Input DataFrame.
+        target_label (int): Numeric label to filter on.
+        max_height (int): Maximum height for filtering.
+        max_width (int): Maximum width for filtering.
 
-    hist_b = cv2.calcHist([b], [0], None, [256], [0, 256])
-    hist_g = cv2.calcHist([g], [0], None, [256], [0, 256])
-    hist_r = cv2.calcHist([r], [0], None, [256], [0, 256])
+    Returns:
+        pd.DataFrame: Filtered DataFrame.
+    """
+    try:
+        filtered_df = input_df.copy()
 
-    plt.figure(figsize=(10, 6))
-    plt.title(f'Histogram for Class {target_label}')
-    plt.plot(hist_b, color='blue', label='Blue Channel')
-    plt.plot(hist_g, color='green', label='Green Channel')
-    plt.plot(hist_r, color='red', label='Red Channel')
-    plt.xlabel('Pixel Value')
-    plt.ylabel('Frequency')
-    plt.legend()
-    plt.show()
+        if target_label is not None:
+            filtered_df = filtered_df[filtered_df['numeric_label']
+                                      == target_label]
+            logging.info(
+                f"Filtered DataFrame for target_label {target_label}.")
 
-    return hist_b, hist_g, hist_r
+        if max_height is not None:
+            filtered_df = filtered_df[filtered_df['height'] <= max_height]
+            logging.info(f"Filtered DataFrame for max_height <= {max_height}.")
+
+        if max_width is not None:
+            filtered_df = filtered_df[filtered_df['width'] <= max_width]
+            logging.info(f"Filtered DataFrame for max_width <= {max_width}.")
+
+        return filtered_df
+    except Exception as e:
+        logging.error(f"Error in filter_dataframe: {e}")
+        return pd.DataFrame()
 
 
-def plot_histogram_matplotlib(hist_b, hist_g, hist_r, target_label):
-    plt.figure(figsize=(15, 8))
+def group_by_label_and_pixel_count(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Group the DataFrame by numeric label and compute pixel count statistics.
 
-    plt.subplot(3, 1, 1)
-    plt.plot(hist_b, color='blue')
-    plt.title(f'Blue Channel Histogram for Class {target_label}')
-    plt.xlabel('Pixel Value')
-    plt.ylabel('Frequency')
+    Parameters:
+        df (pd.DataFrame): Input DataFrame.
 
-    plt.subplot(3, 1, 2)
-    plt.plot(hist_g, color='green')
-    plt.title(f'Green Channel Histogram for Class {target_label}')
-    plt.xlabel('Pixel Value')
-    plt.ylabel('Frequency')
+    Returns:
+        pd.DataFrame: Grouped DataFrame.
+    """
+    try:
+        grouped_df = df.groupby('numeric_label')['pixel_count'].agg(
+            ['min', 'max', 'mean']).reset_index()
+        grouped_df.columns = [
+            'numeric_label', 'min_pixel_count', 'max_pixel_count', 'mean_pixel_count']
 
-    plt.subplot(3, 1, 3)
-    plt.plot(hist_r, color='red')
-    plt.title(f'Red Channel Histogram for Class {target_label}')
-    plt.xlabel('Pixel Value')
-    plt.ylabel('Frequency')
+        logging.info("Grouped DataFrame by numeric label and pixel count.")
+        return grouped_df
+    except Exception as e:
+        logging.error(f"Error in group_by_label_and_pixel_count: {e}")
+        return pd.DataFrame()
 
-    plt.tight_layout()
-    plt.show()
+
+def plot_histogram(dataframe: pd.DataFrame, target_label: int) -> tuple:
+    """
+    Plot histograms for the RGB channels of images in the DataFrame.
+
+    Parameters:
+        dataframe (pd.DataFrame): Input DataFrame.
+        target_label (int): Numeric label for filtering.
+
+    Returns:
+        tuple: Histograms for the Blue, Green, and Red channels.
+    """
+    try:
+        filtered_df = dataframe[dataframe['numeric_label'] == target_label]
+        random_image_path = np.random.choice(
+            filtered_df['absolute_path'].values)
+        image = cv2.imread(random_image_path)
+
+        b, g, r = cv2.split(image)
+
+        hist_b = cv2.calcHist([b], [0], None, [256], [0, 256])
+        hist_g = cv2.calcHist([g], [0], None, [256], [0, 256])
+        hist_r = cv2.calcHist([r], [0], None, [256], [0, 256])
+
+        plt.figure(figsize=(10, 6))
+        plt.title(f'Histogram for Class {target_label}')
+        plt.plot(hist_b, color='blue', label='Blue Channel')
+        plt.plot(hist_g, color='green', label='Green Channel')
+        plt.plot(hist_r, color='red', label='Red Channel')
+        plt.xlabel('Pixel Value')
+        plt.ylabel('Frequency')
+        plt.legend()
+        plt.show()
+
+        logging.info(f"Histogram plotted for Class {target_label}.")
+        return hist_b, hist_g, hist_r
+    except Exception as e:
+        logging.error(f"Error in plot_histogram: {e}")
+        return None, None, None
+
+
+def plot_histogram_matplotlib(hist_b: np.ndarray, hist_g: np.ndarray, hist_r: np.ndarray, target_label: int) -> None:
+    """
+    Plot histograms for the RGB channels using Matplotlib.
+
+    Parameters:
+        hist_b (np.ndarray): Histogram for the Blue channel.
+        hist_g (np.ndarray): Histogram for the Green channel.
+        hist_r (np.ndarray): Histogram for the Red channel.
+        target_label (int): Numeric label for the class.
+    """
+    try:
+        plt.figure(figsize=(15, 8))
+
+        plt.subplot(3, 1, 1)
+        plt.plot(hist_b, color='blue')
+        plt.title(f'Blue Channel Histogram for Class {target_label}')
+        plt.xlabel('Pixel Value')
+        plt.ylabel('Frequency')
+
+        plt.subplot(3, 1, 2)
+        plt.plot(hist_g, color='green')
+        plt.title(f'Green Channel Histogram for Class {target_label}')
+        plt.xlabel('Pixel Value')
+        plt.ylabel('Frequency')
+
+        plt.subplot(3, 1, 3)
+        plt.plot(hist_r, color='red')
+        plt.title(f'Red Channel Histogram for Class {target_label}')
+        plt.xlabel('Pixel Value')
+        plt.ylabel('Frequency')
+
+        plt.tight_layout()
+        plt.show()
+
+        logging.info(f"Histograms plotted for Class {target_label}.")
+    except Exception as e:
+        logging.error(f"Error in plot_histogram_matplotlib: {e}")
 
 
 if __name__ == "__main__":
