8709a8dd30f3ee922de35275387f4bbeac1af2e1
aidenpirs007@mail.ru <aidenpirs007@mail.ru>
2023-12-14 18:04:43 +0400

    Added draw_hist func

--- Lab4/graph.py
+++ Lab4/graph.py
@@ -1,18 +1,18 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-def draw_histograms(histograms) -> None:
+def draw_histograms(hist_blue: np.ndarray, hist_green: np.ndarray, hist_red: np.ndarray) -> None:
+    '''A function for drawing histograms'''
+    fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+
     colors = ['blue', 'green', 'red']
-    channels = ['Blue Channel', 'Green Channel', 'Red Channel']
+    channels = ['Blue', 'Green', 'Red']
 
-    plt.figure(figsize=(12, 8))
-    for i in range(3):
-        plt.subplot(3, 1, i+1)
-        plt.plot(np.arange(256), histograms[i], color=colors[i], label=channels[i])
-        plt.title(f'{channels[i]} Histogram')
-        plt.xlabel('Pixel Intensity')
-        plt.ylabel('Normalized Frequency')
-        plt.legend()
+    for ax, hist, color, channel in zip(axs, [hist_blue, hist_green, hist_red], colors, channels):
+        ax.set_title(f'{channel} Channel Histogram')
+        ax.set_xlabel('Pixel Value')
+        ax.set_ylabel('Frequency')
+        ax.plot(hist, color=color)
 
     plt.tight_layout()
     plt.show()
