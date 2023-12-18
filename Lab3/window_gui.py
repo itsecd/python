e54286d6b522c5145bbaf7abdd9137039fdde919
Arsen Suleymanian <arsens818@gmail.com>
2023-12-18 19:03:44 +0400

    Create file, import functions, make a class MainWindow

---
+++ Lab3/window_gui.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import logging
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QLineEdit, QFileDialog
+from PyQt5.QtGui import QPixmap
+
+sys.path.insert(1, r"C:\Users\Ceh9\PycharmProjects\pythonProject\Lab2")
+from create_annotation import create_annotation_file
+from copy_dataset import copy_and_rename_dataset, generate_random_set
+from iterator import ReviewIterator
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle('Dataset Processing App')
+        self.setGeometry(100, 100, 300, 300)
+
+        self.folder_label = QtWidgets.QLabel('Select Dataset Folder:')
+        self.folder_path = QtWidgets.QLineEdit()
+        self.browse_button = QtWidgets.QPushButton('Browse')
+        self.browse_button.clicked.connect(self.browse_folder)
+
+        self.create_annotation_button = QtWidgets.QPushButton('Create Annotation File')
+        self.create_annotation_button.clicked.connect(self.create_annotation)
+
+        self.copy_dataset_button = QtWidgets.QPushButton('Copy Dataset')
+        self.copy_dataset_button.clicked.connect(self.copy_dataset)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.folder_label)
+        layout.addWidget(self.folder_path)
+        layout.addWidget(self.browse_button)
+        layout.addWidget(self.create_annotation_button)
+        layout.addWidget(self.organize_dataset_button)
+
+        container = QtWidgets.QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
