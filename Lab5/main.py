630d60f29ec799263b86f20dfa78c0b2bbf229d0
Darktezi <Darktezi@mail.ru>
2023-12-18 18:44:54 +0400

    create_sequence_data method

--- Lab5/main.py
+++ Lab5/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import torch
+import torch.nn as nn
 from torch.utils.data import random_split, TensorDataset, DataLoader
 
 def load_and_preprocess_data(file_path):
@@ -25,11 +26,44 @@ def split_data(all_data: torch.Tensor, train_ratio: float = 0.8, val_ratio: floa
 
     return train_data, val_data, test_data
 
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(LSTMModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        
+        out, _ = self.lstm(x, (h0, c0))
+        
+        out = self.fc(out[:, -1, :])
+        return out
+
+def create_sequence_data(data, seq_length):
+    sequences = []
+    labels = []
+
+    for i in range(len(data) - seq_length):
+        seq = data[i:i+seq_length]
+        label = data[i+seq_length:i+seq_length+1]
+        sequences.append(seq)
+        labels.append(label)
+
+    sequences_tensor = torch.stack(sequences)
+    labels_tensor = torch.squeeze(torch.stack(labels))
+
+    return sequences_tensor, labels_tensor
+
 file_path = 'Lab5/dataset.csv'
-all_data = load_and_preprocess_data(file_path)
 
-train_data, val_data, test_data = split_data(all_data)
+data = torch.tensor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+sequences, labels = create_sequence_data(data, seq_length=3)
 
-print(f"Размер обучающей выборки: {len(train_data)}")
-print(f"Размер валидационной выборки: {len(val_data)}")
-print(f"Размер тестовой выборки: {len(test_data)}")
+print("Полученные последовательности:")
+print(sequences)
+print("Соответствующие метки:")
+print(labels)
