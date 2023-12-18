77993624209ae2f03ce300c65f00e36b86c0d6c5
Darktezi <Darktezi@mail.ru>
2023-12-18 18:27:50 +0400

    task 1 and 2

--- Lab5/main.py
+++ Lab5/main.py
@@ -1,18 +1,35 @@
 import pandas as pd
 import torch
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import random_split, TensorDataset, DataLoader
 
-def prepare_data(file_path, batch_size=64):
+def load_and_preprocess_data(file_path):
 
     data = pd.read_csv(file_path)
+    data = data[data['Value'] != "Page not found"]
 
-    data['label'] = data['value'].diff().fillna(0)
+    data['Date'] = pd.to_datetime(data['Date'])
+    data.set_index('Date', inplace=True)
 
-    features_tensor = torch.tensor(data['value'].values, dtype=torch.float32).view(-1, 1) 
-    labels_tensor = torch.tensor(data['label'].values, dtype=torch.float32).view(-1, 1)
+    data['Value'] = (data['Value'].astype(float) - data['Value'].astype(float).min()) / (data['Value'].astype(float).max() - data['Value'].astype(float).min())
 
-    dataset = TensorDataset(features_tensor, labels_tensor)
-    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    return torch.tensor(data['Value'].values, dtype=torch.float32)
 
-    return data_loader
+def split_data(all_data: torch.Tensor, train_ratio: float = 0.8, val_ratio: float = 0.1) -> (torch.utils.data.Dataset, torch.utils.data.Dataset, torch.utils.data.Dataset):
+  
+    train_size = int(train_ratio * len(all_data))
+    val_size = int(val_ratio * len(all_data))
+    test_size = len(all_data) - train_size - val_size
 
+    train_data, valtest_data = random_split(all_data, [train_size, val_size + test_size])
+    val_data, test_data = random_split(valtest_data, [val_size, test_size])
+
+    return train_data, val_data, test_data
+
+file_path = 'Lab5/dataset.csv'
+all_data = load_and_preprocess_data(file_path)
+
+train_data, val_data, test_data = split_data(all_data)
+
+print(f"Размер обучающей выборки: {len(train_data)}")
+print(f"Размер валидационной выборки: {len(val_data)}")
+print(f"Размер тестовой выборки: {len(test_data)}")
